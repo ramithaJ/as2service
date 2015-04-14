14d056088613244e7fc872d3aa411dfd6e13583d
ravi <rasinha@wiley.com>
2015-04-14 15:39:10 +0530

    Admin Login Functionality

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/context/ServiceBeanConfig.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/context/ServiceBeanConfig.java
@@ -3,6 +3,8 @@ package com.wiley.gr.ace.authorservices.services.context;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.wiley.gr.ace.authorservices.services.admin.AdminLoginService;
+import com.wiley.gr.ace.authorservices.services.admin.impl.AdminLoginServiceImpl;
 import com.wiley.gr.ace.authorservices.services.service.UserService;
 import com.wiley.gr.ace.authorservices.services.service.UserServiceImpl;
 
@@ -12,6 +14,11 @@ public class ServiceBeanConfig {
 	@Bean(name = "UserService")
 	public UserService userService() {
 		return new UserServiceImpl();
+	}
+	
+	@Bean(name = "AdminLoginService")
+	public AdminLoginService adminloginservice() {
+		return new AdminLoginServiceImpl();
 	}
 
 }
