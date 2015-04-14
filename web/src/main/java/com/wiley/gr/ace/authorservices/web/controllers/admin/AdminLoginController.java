add4d59548a2878f8e4a87621309bd4a113286d6
ravi <rasinha@wiley.com>
2015-04-14 15:44:40 +0530

    Admin Login Functionality

---
+++ web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/admin/AdminLoginController.java
@@ -0,0 +1,38 @@
+package com.wiley.gr.ace.authorservices.web.controllers.admin;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wiley.gr.ace.authorservices.services.admin.AdminLoginService;
+import com.wiley.gr.ace.authorservices.services.admin.impl.AdminLoginServiceImpl;
+import com.wiley.gr.ace.authorservices.services.context.ServiceBeanConfig;
+
+/**
+ * @author ravisinha
+ *
+ */
+@RestController
+@RequestMapping("/admin")
+public class AdminLoginController {
+	public static ApplicationContext context = new AnnotationConfigApplicationContext(ServiceBeanConfig.class);
+	
+
+	@RequestMapping(value = "/login/{emailId}/{password}", method = RequestMethod.GET, produces = "application/json")
+	public String login(@PathVariable("emailId") String emailId,@PathVariable("password") String password) {
+		
+		boolean status = false;
+		AdminLoginService adminlogin= (AdminLoginServiceImpl)context.getBean("AdminLoginService");
+      	status = adminlogin.validateEmail(emailId);
+      	
+      	if(status) {
+      		return "true";
+      	} else {
+      		return "false";
+      	}
+		
+	}
+}
