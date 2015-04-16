0e328e70d605d322147e35715608876e228e5d3c
Kalyan <kopella.plip@gmail.com>
2015-04-16 15:39:39 +0530

    Added PMD Plugin. Resolved PMD Issues

--- web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/admin/AdminLoginController.java
+++ web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/admin/AdminLoginController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wiley.gr.ace.authorservices.services.admin.AdminLoginService;
-import com.wiley.gr.ace.authorservices.services.admin.external.ALMInterfaceService;
-import com.wiley.gr.ace.authorservices.services.admin.external.impl.ALMInterfaceServiceImpl;
 import com.wiley.gr.ace.authorservices.services.admin.impl.AdminLoginServiceImpl;
 import com.wiley.gr.ace.authorservices.services.context.ServiceBeanConfig;
 
@@ -20,26 +18,28 @@ import com.wiley.gr.ace.authorservices.services.context.ServiceBeanConfig;
 @RestController
 @RequestMapping("/admin")
 public class AdminLoginController {
-	public static ApplicationContext context = new AnnotationConfigApplicationContext(ServiceBeanConfig.class);
-	
+	public static ApplicationContext context = new AnnotationConfigApplicationContext(
+			ServiceBeanConfig.class);
 
 	@RequestMapping(value = "/login/{emailId}/{password}", method = RequestMethod.GET, produces = "application/json")
-	public String login(@PathVariable("emailId") String emailId,@PathVariable("password") String password) {
-		
+	public String login(@PathVariable("emailId") String emailId,
+			@PathVariable("password") String password) {
+
 		boolean status = false;
-		AdminLoginService adminlogin= (AdminLoginServiceImpl)context.getBean("AdminLoginService");
-      	status = adminlogin.validateEmail(emailId);
-      	
-      	if(status) {
-      		
-      		adminlogin.doLogin(emailId, password);
-      	}
-      	
-      	if(status) {
-      		return "true ";
-      	} else {
-      		return "false";
-      	}
-		
+		AdminLoginService adminlogin = (AdminLoginServiceImpl) context
+				.getBean("AdminLoginService");
+		status = adminlogin.validateEmail(emailId);
+
+		if (status) {
+
+			adminlogin.doLogin(emailId, password);
+		}
+
+		if (status) {
+			return "true ";
+		} else {
+			return "false";
+		}
+
 	}
 }
