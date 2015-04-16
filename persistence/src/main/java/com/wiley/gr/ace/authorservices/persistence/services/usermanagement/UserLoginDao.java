e888bb89e452ee838ccac6dfb37f39fff8b0b4b2
ravi <rasinha@wiley.com>
2015-04-16 18:12:20 +0530

    Admin Login Functionality

--- persistence/src/main/java/com/wiley/gr/ace/authorservices/persistence/services/usermanagement/UserLoginDao.java
+++ persistence/src/main/java/com/wiley/gr/ace/authorservices/persistence/services/usermanagement/UserLoginDao.java
@@ -1,30 +1,21 @@
 package com.wiley.gr.ace.authorservices.persistence.services.usermanagement;
 
-import com.wiley.gr.ace.authorservices.persistence.entity.AdminTasks;
-
 public interface UserLoginDao {
-	
+
 	/**
 	 * Checks if emailId exists in USER_PROFILE in AS2.0 database
 	 * 
 	 * @param emailId
 	 */
 	public boolean validateEmail(String emailId);
-	
+
 	/**
-	 * Updates the LAST_LOGIN_TIME, UPDATED_BY, UPDATED_DATE columns of USER_PROFILE table 
+	 * Updates the LAST_LOGIN_TIME, UPDATED_BY, UPDATED_DATE columns of
+	 * USER_PROFILE table
 	 * 
 	 * @param emailId
 	 * @return
 	 */
 	public boolean doLogin(String emailId);
-	
-	/**
-	 * This method adds or updates a record in Admin Task table
-	 * 
-	 * @param uprofile
-	 * @return
-	 */
-	public String addAdminTask(AdminTasks adminTasks);
 
 }
