75060edaf6d40a405da5a9ae716d03ca764b118c
ravi <rasinha@wiley.com>
2015-04-16 18:13:41 +0530

    Admin Login Functionality

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/context/ServiceBeanConfig.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/context/ServiceBeanConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 
 import com.wiley.gr.ace.authorservices.services.admin.AdminLoginService;
 import com.wiley.gr.ace.authorservices.services.admin.external.ALMInterfaceService;
+import com.wiley.gr.ace.authorservices.services.admin.external.BPMInterfaceService;
 import com.wiley.gr.ace.authorservices.services.admin.external.impl.ALMInterfaceServiceImpl;
+import com.wiley.gr.ace.authorservices.services.admin.external.impl.BPMInterfaceServiceImpl;
 import com.wiley.gr.ace.authorservices.services.admin.impl.AdminLoginServiceImpl;
 import com.wiley.gr.ace.authorservices.services.service.UserService;
 import com.wiley.gr.ace.authorservices.services.service.UserServiceImpl;
@@ -17,17 +19,20 @@ public class ServiceBeanConfig {
 	public UserService userService() {
 		return new UserServiceImpl();
 	}
-	
+
 	@Bean(name = "AdminLoginService")
 	public AdminLoginService adminloginservice() {
 		return new AdminLoginServiceImpl();
 	}
-	
-	
+
 	@Bean(name = "ALMExternalService")
-	public ALMInterfaceService aLMExternalService()
-	{
+	public ALMInterfaceService aLMExternalService() {
 		return new ALMInterfaceServiceImpl();
+	}
+
+	@Bean(name = "BPMExternalService")
+	public BPMInterfaceService bpmExternalService() {
+		return new BPMInterfaceServiceImpl();
 	}
 
 }
