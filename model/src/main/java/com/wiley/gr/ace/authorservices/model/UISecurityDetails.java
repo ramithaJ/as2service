be86fd69908d25065e4c0db80dd2827b0f9d7b05
Dishari <dide@wiley.com>
2015-04-17 15:46:25 +0530

    Model add

---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/UISecurityDetails.java
@@ -0,0 +1,46 @@
+package com.wiley.gr.ace.authorservices.model;
+
+/**
+ * @author kpshiva
+ *
+ */
+public class UISecurityDetails {
+
+	private int id1;
+	private int id2;
+	private String answer1;
+	private String answer2;
+
+	public int getId1() {
+		return id1;
+	}
+
+	public void setId1(int id1) {
+		this.id1 = id1;
+	}
+
+	public int getId2() {
+		return id2;
+	}
+
+	public void setId2(int id2) {
+		this.id2 = id2;
+	}
+
+	public String getAnswer1() {
+		return answer1;
+	}
+
+	public void setAnswer1(String answer1) {
+		this.answer1 = answer1;
+	}
+
+	public String getAnswer2() {
+		return answer2;
+	}
+
+	public void setAnswer2(String answer2) {
+		this.answer2 = answer2;
+	}
+
+}
