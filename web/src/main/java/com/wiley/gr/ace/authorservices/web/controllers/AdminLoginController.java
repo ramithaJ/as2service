bc47b43a736ce97a31cccd7a81de1ed72f6e78c3
ravi <rasinha@wiley.com>
2015-04-20 11:58:11 +0530

    Getting Data From Post

--- web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/AdminLoginController.java
+++ web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/AdminLoginController.java
@@ -2,7 +2,9 @@ package com.wiley.gr.ace.authorservices.web.controllers;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,11 @@ import com.wiley.gr.ace.authorservices.services.context.ServiceBeanConfig;
 import com.wiley.gr.ace.authorservices.services.service.AdminLoginService;
 import com.wiley.gr.ace.authorservices.services.service.impl.AdminLoginServiceImpl;
 
-/** This method takes email in encrypted format like --
-	 * kondavinay%40gmail.com/
-	 * Hit the link as http://localhost:8080/author-services/admin/login/kondavinay%40gmail.com/
+/**
+ * This method takes email in encrypted format like -- kondavinay%40gmail.com/
+ * Hit the link as
+ * http://localhost:8080/author-services/admin/login/kondavinay%40gmail.com/
+ * 
  * @author ravisinha
  *
  */
@@ -25,7 +29,8 @@ public class AdminLoginController {
 			ServiceBeanConfig.class);
 
 	@RequestMapping(value = "/login/{emailId}", method = RequestMethod.POST, produces = "application/json")
-	public Service login(@PathVariable("emailId") String emailId) {
+	public Service login(@PathVariable("emailId") String emailId,
+			@RequestBody String password) {
 
 		boolean status = false;
 		Service serviceVO = new Service();
@@ -43,4 +48,3 @@ public class AdminLoginController {
 
 	}
 }
-   
