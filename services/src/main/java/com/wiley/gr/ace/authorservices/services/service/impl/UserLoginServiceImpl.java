53399980631d7e005ebff7279a493abfc6dfdc20
Sarma <skpalli@wiley.com>
2015-04-23 15:56:20 +0530

    Changes for doLogin service

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/UserLoginServiceImpl.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/UserLoginServiceImpl.java
@@ -12,6 +12,7 @@ import com.wiley.gr.ace.authorservices.externalservices.service.ALMInterfaceServ
 import com.wiley.gr.ace.authorservices.externalservices.service.impl.ALMInterfaceServiceImpl;
 import com.wiley.gr.ace.authorservices.model.Security;
 import com.wiley.gr.ace.authorservices.model.Service;
+import com.wiley.gr.ace.authorservices.model.UserMgmt;
 import com.wiley.gr.ace.authorservices.persistence.context.PersistenceBeanConfig;
 import com.wiley.gr.ace.authorservices.persistence.entity.UserSecurityDetails;
 import com.wiley.gr.ace.authorservices.persistence.services.UserLoginServiceDAO;
@@ -36,25 +37,34 @@ public class UserLoginServiceImpl implements UserLoginService {
 
 	
 	@Override
-	public Service doLogin(String emailId, String password) {
+	public UserMgmt doLogin(String emailId, String password) {
 
-		Service service = new Service();
-		if (this.validateEmailAddress(emailId)) {
-			if (this.isUserLocked(emailId)) {
+		UserMgmt userMgmt = null;
+		if (userLoginServiceDAO.validateEmailAddress(emailId)) {
+			
+			if (userLoginServiceDAO.isUserLocked(emailId)) {
+				
 				Date currentDate = new Date();
 				Date date = userLoginServiceDAO.getLockedTime(emailId);
 				Date lockedDate = new Date(date.getTime());
-				return service;
+				if((currentDate.getTime() - lockedDate.getTime()) > 1800000) {
+					// TODO: unlockUser and resetCount
+					if(authenticateUser(emailId, password)) {
+						userMgmt = new UserMgmt();
+						userMgmt.setUserId(userLoginServiceDAO.getUserId(emailId)+"");
+					}
+				}
 			} else {
-				this.authentication(emailId, password);
-				return service;
+				if(authenticateUser(emailId, password)) {
+					userMgmt = new UserMgmt();
+					userMgmt.setUserId(userLoginServiceDAO.getUserId(emailId)+"");
+				}
 			}
 		} else {
-			this.authentication(emailId, password);
-			service.setStatus("Invalid email address. Please Re-Enter");
+			
 			throw new ASException("1001", "Invalid email address. Please Re-Enter");
 		}
-		return service;
+		return userMgmt;
 	}
 
 	@Override
@@ -108,10 +118,8 @@ public class UserLoginServiceImpl implements UserLoginService {
 	@Override
 	public boolean lockUser(String emailId) {
 
-		int records = userLoginServiceDAO.lockUser(emailId);
-		if (records == 1)
-			return true;
-		return false;
+		return userLoginServiceDAO.lockUser(emailId);
+		
 	}
 
 	@Override
@@ -134,28 +142,43 @@ public class UserLoginServiceImpl implements UserLoginService {
 	@Override
 	public Service authentication(String emailId, String password) {
 
+		return null;
+	}
+	
+	/**
+	 * @param emailId
+	 * @param password
+	 * @return
+	 */
+	private boolean authenticateUser(String emailId, String password) {
+		
 		ALMInterfaceService almService = (ALMInterfaceServiceImpl) externalServiceContext
 				.getBean("ALMExternalService");
-		Service service = new Service();
-
+		
+		boolean loginStatus = false;
+		
 		if (almService.authenticateUser(emailId)) {
+			
 			userLoginServiceDAO.doLogin(emailId, password);
-			service.setStatus("successfull login");
-			return service;
+			loginStatus = true;
+			
 		} else {
 			// get count
 			int count = userLoginServiceDAO.getCount(emailId);
 			// increment the count
-			if (count == 3) {
-				if (this.lockUser(emailId))
-					service.setStatus("your account is locked. Please try after some time");
-				return service;
+			if (count >= 2) {
+				
+				if (userLoginServiceDAO.lockUser(emailId))
+					throw new ASException("1002", "Your account is locked. Please try after sometime.");
+				
 			} else {
+				
 				count++;
 				if (userLoginServiceDAO.updateCount(count, emailId))
-					service.setStatus("please enter valid emailID and password");
-				return service;
+					throw new ASException("1003", "Please enter valid EmailId and Password.");
 			}
 		}
+		
+		return loginStatus;
 	}
 }
