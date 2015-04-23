a5647c4119946adb759423200ce339a1d00844ad
Sarma <skpalli@wiley.com>
2015-04-23 13:11:17 +0530

    External service changes

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/UpdateUserServiceImpl.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/UpdateUserServiceImpl.java
@@ -6,11 +6,11 @@ package com.wiley.gr.ace.authorservices.services.service.impl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.wiley.gr.ace.authorservices.externalservices.context.ExternalServiceBeanConfig;
+import com.wiley.gr.ace.authorservices.externalservices.service.ESBInterfaceService;
 import com.wiley.gr.ace.authorservices.model.User;
 import com.wiley.gr.ace.authorservices.persistence.context.PersistenceBeanConfig;
 import com.wiley.gr.ace.authorservices.services.context.ServiceBeanConfig;
-import com.wiley.gr.ace.authorservices.services.external.ESBInterfaceService;
-import com.wiley.gr.ace.authorservices.services.external.impl.ESBInterfaceServiceImpl;
 import com.wiley.gr.ace.authorservices.services.service.UpdateUserService;
 
 /**
@@ -20,8 +20,8 @@ import com.wiley.gr.ace.authorservices.services.service.UpdateUserService;
 public class UpdateUserServiceImpl implements UpdateUserService {
 	
 	private ApplicationContext contextDao = new AnnotationConfigApplicationContext(PersistenceBeanConfig.class);
-	//private ApplicationContext context = new AnnotationConfigApplicationContext(ServiceBeanConfig.class);
-	//private ESBInterfaceService esbInterfaceService = (ESBInterfaceServiceImpl) context.getBean("ESBInterfaceService");
+	private ApplicationContext context = new AnnotationConfigApplicationContext(ExternalServiceBeanConfig.class);
+	private ESBInterfaceService esbInterfaceService = (ESBInterfaceService) context.getBean("ESBInterfaceService");
 
 	/*
 	 * (non-Javadoc)
@@ -36,8 +36,7 @@ public class UpdateUserServiceImpl implements UpdateUserService {
 		 * Fetch Account details and Profile details from external service
 		 * (ESB->ORCID)
 		 */
-		User user = null;
-		//User user = esbInterfaceService.fetchOrcidDetails(orcidId);
+		User user = esbInterfaceService.fetchOrcidDetails(orcidId);
 		if(user!=null){
 			/**
 			 * Update the user account details with ORCID account details
