c90ca3d45eb71746a70c4c00e602ba69ed150593
Sarma <skpalli@wiley.com>
2015-04-23 14:25:41 +0530

    Models for Build User Profile use case

---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/JobCategory.java
@@ -0,0 +1,41 @@
+package com.wiley.gr.ace.authorservices.model;
+
+/**
+ * @author SarmaKumarap
+ *
+ */
+public class JobCategory {
+	
+	private String jobCategoryId;
+	
+	private String jobCategoryName;
+
+	/**
+	 * @return
+	 */
+	public String getJobCategoryId() {
+		return jobCategoryId;
+	}
+
+	/**
+	 * @param jobCategoryId
+	 */
+	public void setJobCategoryId(String jobCategoryId) {
+		this.jobCategoryId = jobCategoryId;
+	}
+
+	/**
+	 * @return
+	 */
+	public String getJobCategoryName() {
+		return jobCategoryName;
+	}
+
+	/**
+	 * @param jobCategoryName
+	 */
+	public void setJobCategoryName(String jobCategoryName) {
+		this.jobCategoryName = jobCategoryName;
+	}
+
+}
