f1e2fb11f4da2f2ec5bea71c762af8cbb384c1dd
Shiva <skpunna@wiley.com>
2015-04-24 17:41:58 +0530

    added ASDataDAOImpl.java

---
+++ persistence/src/main/java/com/wiley/gr/ace/authorservices/persistence/services/impl/ASDataDAOImpl.java
@@ -0,0 +1,13 @@
+package com.wiley.gr.ace.authorservices.persistence.services.impl;
+
+import com.wiley.gr.ace.authorservices.persistence.services.ASDataDAO;
+
+public class ASDataDAOImpl implements ASDataDAO {
+
+	@Override
+	public void getSecurityDetails() {
+
+		
+	}
+
+}
