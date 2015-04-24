41c1f9115773881b0d54148c5ee6886e2d6602bc
Shiva <skpunna@wiley.com>
2015-04-24 17:34:04 +0530

    addes ASDataDAO

---
+++ persistence/src/main/java/com/wiley/gr/ace/authorservices/persistence/services/ASDataDAO.java
@@ -0,0 +1,14 @@
+package com.wiley.gr.ace.authorservices.persistence.services;
+
+/**
+ * @author kpshiva
+ *
+ */
+public interface ASDataDAO {
+	
+	/**
+	 * 
+	 */
+	public void getSecurityDetails();
+
+}
