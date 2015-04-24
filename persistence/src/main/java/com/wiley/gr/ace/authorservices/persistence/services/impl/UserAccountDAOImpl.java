eb0bdd8a202fc56f6d08df41d64fa6d09fbbdced
Shiva <skpunna@wiley.com>
2015-04-24 17:41:39 +0530

    added UserAccountDAOImpl.java

---
+++ persistence/src/main/java/com/wiley/gr/ace/authorservices/persistence/services/impl/UserAccountDAOImpl.java
@@ -0,0 +1,62 @@
+package com.wiley.gr.ace.authorservices.persistence.services.impl;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.wiley.gr.ace.authorservices.persistence.connection.HibernateConnection;
+import com.wiley.gr.ace.authorservices.persistence.context.PersistenceBeanConfig;
+import com.wiley.gr.ace.authorservices.persistence.entity.UserProfile;
+import com.wiley.gr.ace.authorservices.persistence.services.UserAccountDAO;
+
+/**
+ * @author kpshiva
+ *
+ */
+public class UserAccountDAOImpl implements UserAccountDAO {
+
+	private static ApplicationContext context = new AnnotationConfigApplicationContext(
+			PersistenceBeanConfig.class);
+	private static HibernateConnection con = (HibernateConnection) context
+			.getBean("HibernateConnection");
+
+	@Override
+	public UserProfile getEmailDetails(String userId) {
+
+		Session session = null;
+		Transaction transaction = null;
+		try {
+			session = con.getSessionFactory().openSession();
+			transaction = session.beginTransaction();
+			Criteria criteria = session.createCriteria(UserProfile.class);
+			criteria.add(Restrictions.eq("userId", Integer.parseInt(userId)));
+			UserProfile userProfile = (UserProfile) criteria.uniqueResult();
+			System.out.println("shiva" + userProfile.getSecondaryEmailAddr());
+			if (null == userProfile)
+				return null;
+			transaction.commit();
+			return userProfile;
+		} finally {
+			if (session != null) {
+				session.flush();
+				session.close();
+			}
+		}
+	}
+
+	@Override
+	public boolean updateEmailDetails() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean updateSecurityDetails() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
