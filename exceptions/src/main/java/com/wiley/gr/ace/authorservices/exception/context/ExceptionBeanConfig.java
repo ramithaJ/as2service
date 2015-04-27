3b546ce15ef95a1b9cbdb0f5ab6069bf7671c48b
Sarma <skpalli@wiley.com>
2015-04-27 18:36:31 +0530

    Changes done for exception handling

---
+++ exceptions/src/main/java/com/wiley/gr/ace/authorservices/exception/context/ExceptionBeanConfig.java
@@ -0,0 +1,26 @@
+package com.wiley.gr.ace.authorservices.exception.context;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.wiley.gr.ace.authorservices.exception.ASException;
+import com.wiley.gr.ace.authorservices.exception.ASExceptionController;
+
+/**
+ * @author SarmaKumarap
+ *
+ */
+@Configuration
+public class ExceptionBeanConfig {
+	
+	@Bean(name = "asException")
+	public ASException getASExceptionException() {
+		return new ASException();
+	}
+	
+	@Bean(name = "asExceptionController")
+	public ASExceptionController getASExceptionExceptionController() {
+		return new ASExceptionController();
+	}
+	
+}
