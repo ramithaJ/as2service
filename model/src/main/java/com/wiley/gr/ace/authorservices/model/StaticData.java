653b581d5498530a365add1f00e48ee571573410
Shiva <skpunna@wiley.com>
2015-04-28 11:16:14 +0530

    added StaticData model object

---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/StaticData.java
@@ -0,0 +1,32 @@
+package com.wiley.gr.ace.authorservices.model;
+
+/**
+ * This is the generic model for all dropdowns in AS2.0
+ * @author SarmaKumarap
+ *
+ */
+public class StaticData {
+	
+	private String id;
+	
+	private String description;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
