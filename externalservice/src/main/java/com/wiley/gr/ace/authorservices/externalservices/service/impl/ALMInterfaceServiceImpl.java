d7af6ab7feb3596fcd4bfc24f6fc31bae790e7e8
Sarma <skpalli@wiley.com>
2015-04-30 16:38:53 +0530

    Fixed CPD issue in external service

--- externalservice/src/main/java/com/wiley/gr/ace/authorservices/externalservices/service/impl/ALMInterfaceServiceImpl.java
+++ externalservice/src/main/java/com/wiley/gr/ace/authorservices/externalservices/service/impl/ALMInterfaceServiceImpl.java
@@ -11,16 +11,9 @@
  *******************************************************************************/
 package com.wiley.gr.ace.authorservices.externalservices.service.impl;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
-import com.wiley.gr.ace.authorservices.exception.ASException;
+import com.wiley.gr.ace.authorservices.external.util.StubInvokerUtil;
 import com.wiley.gr.ace.authorservices.externalservices.service.ALMInterfaceService;
 import com.wiley.gr.ace.authorservices.model.Service;
 
@@ -37,61 +30,23 @@ public class ALMInterfaceServiceImpl  implements ALMInterfaceService {
 	public boolean authenticateAdminUser(String emailId) {
 		
 		// TODO: Authenticate with LDAP
-				final String url = "http://demo6003007.mockable.io/admin/login";
-				Service service = null;
-				String status = null;
+		final String url = "http://demo6003007.mockable.io/admin/login";
+		Service service = (Service) StubInvokerUtil.invokeStub(url, HttpMethod.POST, "Service");
+		String status = service.getStatus();
 
-				URI uri = null;
-				try {
-					uri = new URI(url);
-					RestTemplate restTemplate = new RestTemplate();
-					HttpHeaders requestHeaders = new HttpHeaders();
-					HttpEntity<Service> requestEntity = new HttpEntity<Service>(
-							requestHeaders);
-
-					ResponseEntity<Service> response = restTemplate.exchange(uri,
-							HttpMethod.POST, requestEntity, Service.class);
-					service = response.getBody();
-					status = service.getStatus();
-					System.out.println("####  " + status);
-
-				} catch (URISyntaxException e) {
-
-					throw new ASException();
-
-				}
-				if (status != null && status.equalsIgnoreCase("success")) {
-					return true;
-				} 
-				return false;
+		if (status != null && status.equalsIgnoreCase("success")) {
+			return true;
+		} 
+		return false;	
 	}
 
 	@Override
 	public boolean authenticateUserALM(String emailId, String password) {
 
 		final String url = "http://demo6003007.mockable.io/user/login";
-		Service service = null;
-		String status = null;
+		Service service = (Service) StubInvokerUtil.invokeStub(url, HttpMethod.POST, "Service");
+		String status = service.getStatus();
 
-		URI uri = null;
-		try {
-			uri = new URI(url);
-			RestTemplate restTemplate = new RestTemplate();
-			HttpHeaders requestHeaders = new HttpHeaders();
-			HttpEntity<Service> requestEntity = new HttpEntity<Service>(
-					requestHeaders);
-
-			ResponseEntity<Service> response = restTemplate.exchange(uri,
-					HttpMethod.POST, requestEntity, Service.class);
-			service = response.getBody();
-			status = service.getStatus();
-			System.out.println("####  " + status);
-
-		} catch (URISyntaxException e) {
-
-			throw new ASException();
-
-		}
 		if (status != null && status.equalsIgnoreCase("success")) {
 			return true;
 		} 
