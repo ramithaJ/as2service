7df137f33c1ba1c4c61f6a55230e6decc75ea698
Sarma <skpalli@wiley.com>
2015-05-08 10:57:58 +0530

    Admin login

---
+++ utils/src/main/java/com/wiley/gr/ace/authorservices/constants/AuthorServicesConstants.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
+ *
+ * All material contained herein is proprietary to John Wiley & Sons 
+ * and its third party suppliers, if any. The methods, techniques and 
+ * technical concepts contained herein are considered trade secrets 
+ * and confidential and may be protected by intellectual property laws.  
+ * Reproduction or distribution of this material, in whole or in part, 
+ * is strictly forbidden except by express prior written permission 
+ * of John Wiley & Sons.
+ *******************************************************************************/
+
+package com.wiley.gr.ace.authorservices.constants;
+
+/**
+ * @author sarmakumarap
+ *
+ */
+public class AuthorServicesConstants {
+	
+	public static final String ADM_ACC_LOOKUP_KEY = "ADMACC";
+
+}
