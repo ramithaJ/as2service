5cd73b6c8d3a2ed0609b676a1b60b73941d6babb
Sarma <skpalli@wiley.com>
2015-05-11 20:06:17 +0530

    AS2-5:11489-Create User Role

---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/RolePermissionMapping.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
+ *
+ * All material contained herein is proprietary to John Wiley & Sons 
+ * and its third party suppliers, if any. The methods, techniques and 
+ * technical concepts contained herein are considered trade secrets 
+ * and confidential and may be protected by intellectual property laws.  
+ * Reproduction or distribution of this material, in whole or in part, 
+ * is strictly forbidden except by express prior written permission 
+ * of John Wiley & Sons.
+ *******************************************************************************/
+
+package com.wiley.gr.ace.authorservices.model;
+
+
+
+/**
+ * @author sarmakumarap
+ *
+ */
+public class RolePermissionMapping {
+	
+	private String roleId;
+	
+	private StringBuffer permissionsList;
+	
+
+	/**
+	 * @return
+	 */
+	/**
+	 * @return
+	 */
+	public String getRoleId() {
+		return roleId;
+	}
+
+	/**
+	 * @param roleId
+	 */
+	/**
+	 * @param roleId
+	 */
+	public void setRoleId(String roleId) {
+		this.roleId = roleId;
+	}
+
+	/**
+	 * @return
+	 */
+	public StringBuffer getPermissionsList() {
+		return permissionsList;
+	}
+
+	/**
+	 * @param permissionsList
+	 */
+	public void setPermissionsList(StringBuffer permissionsList) {
+		this.permissionsList = permissionsList;
+	}
+	
+	
+
+}
---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/RolesAndPermissions.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
+ *
+ * All material contained herein is proprietary to John Wiley & Sons 
+ * and its third party suppliers, if any. The methods, techniques and 
+ * technical concepts contained herein are considered trade secrets 
+ * and confidential and may be protected by intellectual property laws.  
+ * Reproduction or distribution of this material, in whole or in part, 
+ * is strictly forbidden except by express prior written permission 
+ * of John Wiley & Sons.
+ *******************************************************************************/
+
+package com.wiley.gr.ace.authorservices.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author sarmakumarap
+ *
+ */
+public class RolesAndPermissions {
+	
+	private List<Role> rolesList = new ArrayList<Role>();
+	
+	private List<PermissionSection> sectionsList = new ArrayList<PermissionSection>();
+	
+	private Map<String, StringBuffer> permissionsMap = new HashMap<String, StringBuffer>();
+
+	/**
+	 * @return
+	 */
+	public List<Role> getRolesList() {
+		return rolesList;
+	}
+
+	/**
+	 * @param rolesList
+	 */
+	public void setRolesList(List<Role> rolesList) {
+		this.rolesList = rolesList;
+	}
+
+	/**
+	 * @return
+	 */
+	public List<PermissionSection> getSectionsList() {
+		return sectionsList;
+	}
+
+	/**
+	 * @param sectionsList
+	 */
+	public void setSectionsList(List<PermissionSection> sectionsList) {
+		this.sectionsList = sectionsList;
+	}
+
+	/**
+	 * @return
+	 */
+	public Map<String, StringBuffer> getPermissionsMap() {
+		return permissionsMap;
+	}
+
+	/**
+	 * @param permissionsMap
+	 */
+	public void setPermissionsMap(Map<String, StringBuffer> permissionsMap) {
+		this.permissionsMap = permissionsMap;
+	}
+
+
+}
