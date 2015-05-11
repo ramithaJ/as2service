f5f35f0039f44b44529bbab91302a781c1a155df
Sarma <skpalli@wiley.com>
2015-05-11 20:08:25 +0530

    AS2-5:11489-Create User Role

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/AdminLoginServiceImpl.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/AdminLoginServiceImpl.java
@@ -11,10 +11,23 @@
  *******************************************************************************/
 package com.wiley.gr.ace.authorservices.services.service.impl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.wiley.gr.ace.authorservices.constants.AuthorServicesConstants;
 import com.wiley.gr.ace.authorservices.externalservices.service.ALMInterfaceService;
 import com.wiley.gr.ace.authorservices.externalservices.service.BPMInterfaceService;
+import com.wiley.gr.ace.authorservices.model.PermissionSection;
+import com.wiley.gr.ace.authorservices.model.Role;
+import com.wiley.gr.ace.authorservices.model.RolesAndPermissions;
+import com.wiley.gr.ace.authorservices.model.UserPermissions;
+import com.wiley.gr.ace.authorservices.persistence.entity.Permissions;
+import com.wiley.gr.ace.authorservices.persistence.entity.RolePermissions;
+import com.wiley.gr.ace.authorservices.persistence.entity.Roles;
+import com.wiley.gr.ace.authorservices.persistence.services.ASDataDAO;
 import com.wiley.gr.ace.authorservices.persistence.services.UserLoginDao;
 import com.wiley.gr.ace.authorservices.services.service.AdminLoginService;
 
@@ -32,7 +45,8 @@ public class AdminLoginServiceImpl implements AdminLoginService {
 	ALMInterfaceService almService;
 	@Autowired(required=true)
 	BPMInterfaceService bpmService;
-	
+	@Autowired(required=true)
+	ASDataDAO  asDataDAO;
 
 	/*
 	 * (non-Javadoc)
@@ -71,6 +85,82 @@ public class AdminLoginServiceImpl implements AdminLoginService {
 	
 		// TODO: Integrate with BPM Service
 		return bpmService.createTask();
+	}
+
+	@Override
+	public RolesAndPermissions getRolesAndPermissions(String roleId) {
+		
+		RolesAndPermissions rolesAndPermissions = new RolesAndPermissions();
+		
+		Map<String, StringBuffer> permissionsMap = new HashMap();
+		
+		PermissionSection systemSection = new PermissionSection();
+		PermissionSection articleSection = new PermissionSection();
+		PermissionSection adminSection = new PermissionSection();
+		
+		systemSection.setSectionName(AuthorServicesConstants.PERMISSION_LEVEL_SYSTEM);
+		articleSection.setSectionName(AuthorServicesConstants.PERMISSION_LEVEL_ARTICLE);
+		adminSection.setSectionName(AuthorServicesConstants.PERMISSION_LEVEL_ADMIN);
+		
+		if(roleId == null || roleId.equals("")) {
+			
+			List<Roles> daoRolesList = asDataDAO.getAdminRoles();
+			
+			for (Roles daoRoles : daoRolesList) {
+				
+				Role role = new Role();
+				role.setRoleId(daoRoles.getRoleId()+"");
+				role.setRoleName(daoRoles.getRoleName());
+				rolesAndPermissions.getRolesList().add(role);
+			}
+			
+			List<Permissions> daoPermissionsList = asDataDAO.getPermissions();
+			
+			for (Permissions daoPermissions : daoPermissionsList) {
+				
+				UserPermissions permission = new UserPermissions();
+				
+				permission.setPermissionId(daoPermissions.getPermissionId()+"");
+				permission.setPermissionName(daoPermissions.getPermissionName());
+				
+				if(daoPermissions.getLevelOfPermission().equalsIgnoreCase(AuthorServicesConstants.PERMISSION_LEVEL_SYSTEM)
+						&& daoPermissions.getPermType().equalsIgnoreCase(AuthorServicesConstants.PERMISSION_TYPE_EXTERNAL)) {
+					systemSection.getPermissionsList().add(permission);
+				
+				} else if(daoPermissions.getLevelOfPermission().equalsIgnoreCase(AuthorServicesConstants.PERMISSION_LEVEL_SYSTEM)
+						&& daoPermissions.getPermType().equalsIgnoreCase(AuthorServicesConstants.PERMISSION_TYPE_INTERNAL)) {
+					adminSection.getPermissionsList().add(permission);
+				
+				} else if(daoPermissions.getLevelOfPermission().equalsIgnoreCase(AuthorServicesConstants.PERMISSION_LEVEL_ARTICLE))  {
+					articleSection.getPermissionsList().add(permission);
+				}
+			}
+			
+			rolesAndPermissions.getSectionsList().add(adminSection);
+			rolesAndPermissions.getSectionsList().add(articleSection);
+			rolesAndPermissions.getSectionsList().add(systemSection);
+			
+			List<RolePermissions> daoPermissionMappings = asDataDAO.getRolePermissionMappings();
+			
+			for (RolePermissions daoRolePermissions : daoPermissionMappings) {
+				
+				String roleIdString = daoRolePermissions.getId().getRoleId()+"";
+				if(permissionsMap.containsKey(roleIdString)) {
+					StringBuffer permissionsString = permissionsMap.get(roleIdString);
+					if(permissionsString == null) {
+						permissionsString = new StringBuffer();
+						permissionsString.append(daoRolePermissions.getId().getPermissionId()+"");
+					} else {
+						permissionsString.append(", "+daoRolePermissions.getId().getPermissionId()+"");
+					}
+				} else {
+					permissionsMap.put(roleIdString, new StringBuffer().append(daoRolePermissions.getId().getPermissionId()+""));
+				}
+				
+			}
+			rolesAndPermissions.setPermissionsMap(permissionsMap);
+		}
+		return rolesAndPermissions;
 	}
 
 }
