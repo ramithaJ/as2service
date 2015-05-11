3caf15173d553dc66ec3900053d2c4dd2669df01
Shiva <skpunna@wiley.com>
2015-05-11 13:19:51 +0530

    model changes < AS2-1 | skpunna | user Log-in management changes >

---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/SecurityDetails.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
+ *
+ * All material contained herein is proprietary to John Wiley & Sons 
+ * and its third party suppliers, if any. The methods, techniques and 
+ * technical concepts contained herein are considered trade secrets 
+ * and confidential and may be protected by intellectual property laws.  
+ * Reproduction or distribution of this material, in whole or in part, 
+ * is strictly forbidden except by express prior written permission 
+ * of John Wiley & Sons.
+ *******************************************************************************/
+package com.wiley.gr.ace.authorservices.model;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+/**
+ * @author kpshiva
+ */
+@JsonInclude(Include.NON_NULL)
+public class Security {
+
+	@NotNull @NotBlank
+	private String securityQuestionId;
+	
+	@NotNull @NotBlank
+	private String securityQuestion;
+
+	@NotNull @NotBlank
+	private String securityAnswer;
+
+	public String getSecurityQuestion() {
+		return securityQuestion;
+	}
+
+	public void setSecurityQuestion(String securityQuestion) {
+		this.securityQuestion = securityQuestion;
+	}
+
+	public String getSecurityAnswer() {
+		return securityAnswer;
+	}
+
+	public void setSecurityAnswer(String securityAnswer) {
+		this.securityAnswer = securityAnswer;
+	}
+
+	public String getSecurityQuestionId() {
+		return securityQuestionId;
+	}
+
+	public void setSecurityQuestionId(String securityQuestionId) {
+		this.securityQuestionId = securityQuestionId;
+	}
+
+
+
+}
