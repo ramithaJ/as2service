052e22ebf8a711c2b2bafb0bda67afea21a946d5
Shiva <skpunna@wiley.com>
2015-05-12 18:26:54 +0530

    AuthorCoAuthorDAO changes < AS2-6 | skpunna | user Account changes >

--- persistence/src/main/java/com/wiley/gr/ace/authorservices/persistence/services/AuthorCoAuthorDAO.java
+++ persistence/src/main/java/com/wiley/gr/ace/authorservices/persistence/services/AuthorCoAuthorDAO.java
@@ -2,10 +2,33 @@ package com.wiley.gr.ace.authorservices.persistence.services;
 
 import java.util.List;
 
+import com.wiley.gr.ace.authorservices.model.CoAuthor;
 import com.wiley.gr.ace.authorservices.persistence.entity.AuthCoauthDetails;
 
+/**
+ * @author kpshiva
+ *
+ */
 public interface AuthorCoAuthorDAO {
-	
+
+	/**
+	 * @param userId
+	 * @return
+	 */
 	List<AuthCoauthDetails> getCoAuthorsList(String userId);
+
+	/**
+	 * @param userId
+	 * @param coAuthorJson
+	 * @return
+	 */
+	boolean editCoAuthor(String userId, CoAuthor coAuthorJson);
+
+	/**
+	 * @param userId
+	 * @param coAuthorId
+	 * @return
+	 */
+	boolean deleteCoAuthor(String userId, String coAuthorId);
 
 }
