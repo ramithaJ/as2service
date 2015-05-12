4759cb90664f9b9840e27f5653dcc83a568311fb
Shiva <skpunna@wiley.com>
2015-05-12 18:25:28 +0530

    co author changes < AS2-6 | skpunna | user Account changes >

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/service/UserProfileService.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/UserProfileService.java
@@ -26,161 +26,157 @@ import com.wiley.gr.ace.authorservices.model.Society;
  *
  */
 public interface UserProfileService {
-	
+
 	/**
 	 * @param userId
 	 * @return
 	 */
-	 Affiliation[] getAffiliationsForUser(String userId);
-	
-	/**
-	 * @param userId
-	 * @param affiliation
-	 * @return
-	 */
-	 boolean addAffiliations(String userId, Affiliation affiliation);
-	
+	Affiliation[] getAffiliationsForUser(String userId);
+
 	/**
 	 * @param userId
 	 * @param affiliation
 	 * @return
 	 */
-	 boolean updateAffiliation(String userId, Affiliation affiliation);
-	
+	boolean addAffiliations(String userId, Affiliation affiliation);
+
+	/**
+	 * @param userId
+	 * @param affiliation
+	 * @return
+	 */
+	boolean updateAffiliation(String userId, Affiliation affiliation);
+
 	/**
 	 * @param userId
 	 * @param affiliationId
 	 * @return
 	 */
-	 boolean deleteAffiliation(String userId, String affiliationId);
-	
+	boolean deleteAffiliation(String userId, String affiliationId);
+
 	/**
 	 * @param userId
 	 * @return
 	 */
-	 ResearchFunder[] getResearchFunders(String userId);
-	
-	/**
-	 * @param userId
-	 * @param researchFunder
-	 * @return
-	 */
-	 boolean addResearchFunder(String userId, ResearchFunder researchFunder);
-	
+	ResearchFunder[] getResearchFunders(String userId);
+
 	/**
 	 * @param userId
 	 * @param researchFunder
 	 * @return
 	 */
-	 boolean updateResearchFunder(String userId, ResearchFunder researchFunder);
-	
+	boolean addResearchFunder(String userId, ResearchFunder researchFunder);
+
+	/**
+	 * @param userId
+	 * @param researchFunder
+	 * @return
+	 */
+	boolean updateResearchFunder(String userId, ResearchFunder researchFunder);
+
 	/**
 	 * @param userId
 	 * @param researchFunderId
 	 * @return
 	 */
-	 boolean deleteResearchFunder(String userId, String researchFunderId);
-	
+	boolean deleteResearchFunder(String userId, String researchFunderId);
+
 	/**
 	 * @param userId
 	 * @return
 	 */
-	 Society[]  getSocietiesForUser(String userId);
-	
+	Society[] getSocietiesForUser(String userId);
+
 	/**
 	 * @param userId
 	 * @param societyId
 	 * @return
 	 */
-	 Society getSocietyDetails(String userId, String societyId);
-	
+	Society getSocietyDetails(String userId, String societyId);
+
 	/**
 	 * @param userId
 	 * @param society
 	 * @return
 	 */
-	 boolean updateSocietyDetails(String userId, Society society);
-	
+	boolean updateSocietyDetails(String userId, Society society);
+
 	/**
 	 * @param userId
 	 * @param societyId
 	 * @return
 	 */
-	 boolean deleteSociety(String userId, String societyId);
-	
-	
+	boolean deleteSociety(String userId, String societyId);
+
 	/**
 	 * @param userId
 	 * @return
 	 */
-	 Interests getInterests(String userId);
-	
+	Interests getInterests(String userId);
+
 	/**
 	 * @param userId
 	 * @return
 	 */
-	 boolean addInterest(Interests interest);
-	
+	boolean addInterest(Interests interest);
+
 	/**
 	 * @param userId
 	 * @return
 	 */
-	 boolean removeInterest(String interestId);
-	
-	
+	boolean removeInterest(String interestId);
+
 	/**
 	 * @return
 	 */
-	 Interests[] searchInterests();
-	
+	Interests[] searchInterests();
+
 	/**
 	 * @param userId
 	 * @return
 	 */
-	 CoAuthor[] getCoAuthorsList(String userId);
-	
+	CoAuthor[] getCoAuthorsList(String userId);
+
 	/**
 	 * @param coAuthor
 	 * @return
 	 */
-	 boolean editCoAuthor(CoAuthor coAuthor);
-	
+	boolean editCoAuthor(String userId, CoAuthor coAuthorJson);
+
 	/**
 	 * @param coAuthorId
 	 * @return
 	 */
-	 boolean deleteCoAuthor(String coAuthorId);
-	
+	boolean deleteCoAuthor(String userId, String coAuthorId);
+
 	/**
 	 * @param userId
 	 * @return
 	 */
-	 PreferredJournals[] getPreferredJournals(String userId);
-	
+	PreferredJournals[] getPreferredJournals(String userId);
+
 	/**
 	 * @param userId
 	 * @param journalId
 	 * @return
 	 */
-	 boolean deletePreferredJournals(String userId, String journalId);
-	
+	boolean deletePreferredJournals(String userId, String journalId);
+
 	/**
 	 * @return
 	 */
-	 String searchJournals();
-	
+	String searchJournals();
+
 	/**
 	 * @param userId
 	 * @return
 	 */
-	 Alert[] getListOfAlerts(String userId);
-	
+	Alert[] getListOfAlerts(String userId);
+
 	/**
 	 * @param alerts
 	 * @return
 	 */
-	 boolean updateAlerts(String userId,List<Alert> alertsList );
-	
-	
+	boolean updateAlerts(String userId, List<Alert> alertsList);
 
 }
