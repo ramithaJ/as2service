927bcb27b0c3978e9fd76b208c2ce2aa2b3d6f20
yugandhar <ykandula@wiley.com>
2015-05-14 19:02:52 +0530

    AS-21|yugandhark|DashBoard.java class is added for Providing Service from Controller

---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/DashBoard.java
@@ -0,0 +1,141 @@
+package com.wiley.gr.ace.authorservices.model;
+
+/**
+ * @author Virtusa
+ *
+ */
+public class DashBoard {
+
+	/** The security Details . */
+	private SecurityDetails securityDetails;
+
+	/** The service funders . */
+	private Service serviceFunders;
+
+	/** The userMgmt . */
+	private UserMgmt userMgmt;
+
+	/** The society Details . */
+	private Society society;
+
+	/** The Affiliations  . */
+	private Affiliation affiliation;
+
+	/** The interests . */
+	private Interests interests;
+
+	/**
+	 * Gets the Security Details.
+	 *
+	 * @return the Security Details
+	 */
+	public SecurityDetails getSecurityDetails() {
+		return securityDetails;
+	}
+
+	/**
+	 * Sets the Security Details.
+	 *
+	 * @param the
+	 *            Security Details
+	 */
+	public void setSecurityDetails(SecurityDetails securityDetails) {
+		this.securityDetails = securityDetails;
+	}
+
+	/**
+	 * Gets the Funders Details.
+	 *
+	 * @return the Funders Details
+	 */
+	public Service getServiceFunders() {
+		return serviceFunders;
+	}
+
+	/**
+	 * sets the Funders Details.
+	 *
+	 * @param the
+	 *            Funders
+	 */
+	public void setServiceFunders(Service serviceFunders) {
+		this.serviceFunders = serviceFunders;
+	}
+
+	/**
+	 * Gets the UserMgmt.
+	 *
+	 * @return the UserMgmt
+	 */
+	public UserMgmt getUserMgmt() {
+		return userMgmt;
+	}
+
+	/**
+	 * Sets the UserMgmt.
+	 *
+	 * @param the
+	 *            UserMgmt
+	 */
+	public void setUserMgmt(UserMgmt userMgmt) {
+		this.userMgmt = userMgmt;
+	}
+
+	/**
+	 * Gets the Society.
+	 *
+	 * @return the Society
+	 */
+	public Society getSociety() {
+		return society;
+	}
+
+	/**
+	 * Sets the Society.
+	 *
+	 * @param the
+	 *            Society
+	 */
+	public void setSociety(Society society) {
+		this.society = society;
+	}
+
+	/**
+	 * Gets the affiliation.
+	 *
+	 * @return the affiliation
+	 */
+	public Affiliation getAffiliation() {
+		return affiliation;
+	}
+
+	/**
+	 * Sets the affiliation.
+	 *
+	 * @param the
+	 *            affiliation
+	 */
+	public void setAffiliation(Affiliation affiliation) {
+		this.affiliation = affiliation;
+	}
+
+	/**
+	 * Gets the Interests.
+	 *
+	 * @return the Interests
+	 */
+	public Interests getInterests() {
+		return interests;
+	}
+
+	/**
+	 * Sets the Interests.
+	 *
+	 * @param the
+	 *            Interests
+	 */
+	public void setInterests(Interests interests) {
+		this.interests = interests;
+	}
+
+}
