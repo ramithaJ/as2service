8baccfa8f5955ae0ae61ee2eb98ac7c3755bdcde
yugandhar <ykandula@wiley.com>
2015-05-14 19:06:23 +0530

    AS-21|yugandhark|Code Changes

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/DashBoardServiceImpl.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/DashBoardServiceImpl.java
@@ -11,14 +11,18 @@
  *******************************************************************************/
 package com.wiley.gr.ace.authorservices.services.service.impl;
 
-import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 
+import com.wiley.gr.ace.authorservices.model.Affiliation;
+import com.wiley.gr.ace.authorservices.model.DashBoard;
+import com.wiley.gr.ace.authorservices.model.Interests;
 import com.wiley.gr.ace.authorservices.model.SecurityDetails;
 import com.wiley.gr.ace.authorservices.model.Service;
+import com.wiley.gr.ace.authorservices.model.Society;
+import com.wiley.gr.ace.authorservices.model.UserMgmt;
 import com.wiley.gr.ace.authorservices.persistence.entity.AuthorProfile;
 import com.wiley.gr.ace.authorservices.persistence.entity.UserFunderGrants;
 import com.wiley.gr.ace.authorservices.persistence.entity.UserSecurityDetails;
@@ -38,23 +42,23 @@ public class DashBoardServiceImpl implements DashBoardService {
 
 	/**
 	 * @param userId to get the data from DashBoardDAO
-	 * @return dashBoardProfileList
+	 * @return dashBoard
 	 */
-	public final List<Service> getProfileMeter(final int userId) {
-		List<Service> dashBoardProfileList = new LinkedList<Service>();
+	public final DashBoard getProfileMeter(final int userId) {
 		List<UserSecurityDetails> securityDetailsList = null;
+		DashBoard dashBoard = new DashBoard();
 		securityDetailsList = dashBoardDAO.getSecurityDetailsList(userId);
 		if (null != securityDetailsList) {
 			for (UserSecurityDetails secureDetails : securityDetailsList) {
-				SecurityDetails security = new SecurityDetails();
-				security.setSecurityQuestion(secureDetails
-						.getSecurityQuestion());
-				security.setSecurityAnswer(secureDetails.getSecurityAnswer());
-				if (StringUtils.isEmpty(security.getSecurityQuestion())
-						|| StringUtils.isEmpty(security.getSecurityAnswer())) {
-					Service service = new Service();
-					service.setStatus("Please Fill  Questions and Answers");
-					dashBoardProfileList.add(service);
+				if (StringUtils.isEmpty(secureDetails.getSecurityQuestion())
+						|| StringUtils.isEmpty(secureDetails
+								.getSecurityAnswer())) {
+					SecurityDetails securityDetails = new SecurityDetails();
+					securityDetails.setSecurityQuestion(secureDetails
+							.getSecurityQuestion());
+					securityDetails.setSecurityAnswer(secureDetails
+							.getSecurityAnswer());
+					dashBoard.setSecurityDetails(securityDetails);
 					break;
 				}
 			}
@@ -64,15 +68,26 @@ public class DashBoardServiceImpl implements DashBoardService {
 		if (StringUtils.isEmpty(userFunderGrants)) {
 			Service service = new Service();
 			service.setStatus("Missed to Add Your Funder Details ");
-			dashBoardProfileList.add(service);
+			dashBoard.setServiceFunders(service);
 		}
 		AuthorProfile authorMissedProfile = dashBoardDAO
 				.getMissedUserProfile(userId);
-			if (authorMissedProfile.getIsAccountVerified().equals('N')) {
-				Service service = new Service();
-				service.setStatus("Your Account is Not Verified And setup OrcidId and SecondaryEmail Address");
-				dashBoardProfileList.add(service);
-			}
-		return dashBoardProfileList;
+		Character isAccountVerified = authorMissedProfile
+				.getIsAccountVerified();
+		if (isAccountVerified.equals('N')) {
+			UserMgmt userMgmt = new UserMgmt();
+			userMgmt.setIsAccountVerified(isAccountVerified);
+			userMgmt.setOrcidID("OrcidId");
+			userMgmt.setSecondaryEmailAddress("secondaryEmailAddr");
+			dashBoard.setUserMgmt(userMgmt);
+		}
+		Society society = new Society();
+		society.setSocietyId("false");
+		dashBoard.setSociety(society);
+		Affiliation affiliation = new Affiliation();
+		dashBoard.setAffiliation(affiliation);
+		Interests interests = new Interests();
+		dashBoard.setInterests(interests);
+		return dashBoard;
 	}
 }
