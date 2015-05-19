0dc8e6ab7f4ab89d261cd1cd95abc4d93b11455d
ravi <rasinha@wiley.com>
2015-05-19 13:26:38 +0530

    Added New External Service Impl

---
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/AuthorProfileService.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
+ *
+ * All material contained herein is proprietary to John Wiley & Sons 
+ * and its third party suppliers, if any. The methods, techniques and 
+ * technical concepts contained herein are considered trade secrets 
+ * and confidential and may be protected by intellectual property laws.  
+ * Reproduction or distribution of this material, in whole or in part, 
+ * is strictly forbidden except by express prior written permission 
+ * of John Wiley & Sons.
+ *******************************************************************************/
+
+package com.wiley.gr.ace.authorservices.services.service;
+
+import java.util.List;
+
+import com.wiley.gr.ace.authorservices.model.Affiliation;
+import com.wiley.gr.ace.authorservices.model.Alert;
+import com.wiley.gr.ace.authorservices.model.ResearchFunder;
+import com.wiley.gr.ace.authorservices.model.Society;
+
+public interface AuthorProfileService {
+
+	boolean updateSocietyDetails(Society society);
+
+	boolean updateAffiliation(Affiliation affiliation);
+
+	boolean updateResearchFunder(String userId,
+			ResearchFunder researchFunder);
+
+	boolean updateAlerts(String userId, List<Alert> alertsList);
+
+	boolean UpdatecoAuthor(String userId);
+
+}
---
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/AuthorProfileServiceImpl.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
+ *
+ * All material contained herein is proprietary to John Wiley & Sons 
+ * and its third party suppliers, if any. The methods, techniques and 
+ * technical concepts contained herein are considered trade secrets 
+ * and confidential and may be protected by intellectual property laws.  
+ * Reproduction or distribution of this material, in whole or in part, 
+ * is strictly forbidden except by express prior written permission 
+ * of John Wiley & Sons.
+ *******************************************************************************/
+
+package com.wiley.gr.ace.authorservices.services.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.wiley.gr.ace.authorservices.externalservices.service.CDMInterfaceService;
+import com.wiley.gr.ace.authorservices.model.Affiliation;
+import com.wiley.gr.ace.authorservices.model.Alert;
+import com.wiley.gr.ace.authorservices.model.ResearchFunder;
+import com.wiley.gr.ace.authorservices.model.Society;
+import com.wiley.gr.ace.authorservices.model.UserProfile;
+import com.wiley.gr.ace.authorservices.model.external.LookUpProfile;
+import com.wiley.gr.ace.authorservices.services.service.AuthorProfileService;
+
+public class AuthorProfileServiceImpl implements AuthorProfileService {
+
+	@Autowired
+	CDMInterfaceService cdmservices;
+
+	@Override
+	public boolean updateSocietyDetails(Society society) {
+		UserProfile userProfile = new UserProfile();
+		LookUpProfile lookUpProfile = new LookUpProfile();
+		userProfile.setSocieties((List<Society>) society);
+		lookUpProfile.setUserProfile(userProfile);
+		cdmservices.updateProfile(lookUpProfile);
+
+		return true;
+	}
+
+	@Override
+	public boolean updateAffiliation(Affiliation affiliation) {
+		UserProfile userProfile = new UserProfile();
+		LookUpProfile lookUpProfile = new LookUpProfile();
+
+		userProfile.setAffiliations((List<Affiliation>) affiliation);
+		lookUpProfile.setUserProfile(userProfile);
+		cdmservices.updateProfile(lookUpProfile);
+
+		return true;
+	}
+
+	@Override
+	public boolean updateResearchFunder(String userId,
+			ResearchFunder researchFunder) {
+		UserProfile userProfile = new UserProfile();
+		LookUpProfile lookUpProfile = new LookUpProfile();
+		userProfile.setResearchFunders((List<ResearchFunder>) researchFunder);
+		lookUpProfile.setUserProfile(userProfile);
+		cdmservices.updateProfile(lookUpProfile);
+
+		return true;
+	}
+
+	@Override
+	public boolean updateAlerts(String userId, List<Alert> alertsList) {
+		UserProfile userProfile = new UserProfile();
+		LookUpProfile lookUpProfile = new LookUpProfile();
+		userProfile.setAlerts(alertsList);
+		lookUpProfile.setUserProfile(userProfile);
+		cdmservices.updateProfile(lookUpProfile);
+		return true;
+	}
+
+	@Override
+	public boolean UpdatecoAuthor(String userId) {
+		UserProfile userProfile = new UserProfile();
+		LookUpProfile lookUpProfile = new LookUpProfile();
+		lookUpProfile.setUserProfile(userProfile);
+		cdmservices.updateProfile(lookUpProfile);
+		return true;
+	}
+
+}
