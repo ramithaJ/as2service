6910b6dcd47d57517ba78f45da05adfb97fced0b
Sarma <skpalli@wiley.com>
2015-05-19 14:47:57 +0530

    AS2-5:11489-Create User Role

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/AdminLoginServiceImpl.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/AdminLoginServiceImpl.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.StringTokenizer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
