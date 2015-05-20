fefef0d5f6dba186af768258ca31067dc5e44afc
yugandhar <ykandula@wiley.com>
2015-05-20 20:52:50 +0530

    AS-21|yugandhark|Model Class is added For DashBoard

--- model/src/main/java/com/wiley/gr/ace/authorservices/model/DashBoard.java
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/DashBoard.java
@@ -1,144 +1,60 @@
 package com.wiley.gr.ace.authorservices.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 /**
  * @author Virtusa
+ *
  */
 @JsonInclude(Include.NON_NULL)
 public class DashBoard {
-    
-    /** The security Details . */
-    private SecurityDetails securityDetails;
-    
-    /** The user funders . */
-    private UserFunder userFunders;
-    
-    /** The userMgmt . */
-    private User user;
-    
-    /** The society Details . */
-    private Society society;
-    
-    /** The Affiliations . */
-    private Affiliation affiliation;
-    
-    /** The interests . */
-    private Interests interests;
-    
-    /**
-     * Gets the Security Details.
-     *
-     * @return the Security Details
-     */
-    public SecurityDetails getSecurityDetails() {
-        return securityDetails;
-    }
-    
-    /**
-     * Sets the Security Details.
-     *
-     * @param the
-     *            Security Details
-     */
-    public void setSecurityDetails(SecurityDetails securityDetails) {
-        this.securityDetails = securityDetails;
-    }
-    
-    /**
-     * Gets the Funders Details.
-     *
-     * @return the Funders Details
-     */
-    public UserFunder getUserFunders() {
-        return userFunders;
-    }
-    
-    /**
-     * sets the Funders Details.
-     *
-     * @param the
-     *            Funders
-     */
-    public void setUserFunders(UserFunder userFunders) {
-        this.userFunders = userFunders;
-    }
-    
-    /**
-     * Gets the UserMgmt.
-     *
-     * @return the UserMgmt
-     */
-    public User getUser() {
-        return user;
-    }
-    
-    /**
-     * Sets the UserMgmt.
-     *
-     * @param the
-     *            UserMgmt
-     */
-    public void setUser(User user) {
-        this.user = user;
-    }
-    
-    /**
-     * Gets the Society.
-     *
-     * @return the Society
-     */
-    public Society getSociety() {
-        return society;
-    }
-    
-    /**
-     * Sets the Society.
-     *
-     * @param the
-     *            Society
-     */
-    public void setSociety(Society society) {
-        this.society = society;
-    }
-    
-    /**
-     * Gets the affiliation.
-     *
-     * @return the affiliation
-     */
-    public Affiliation getAffiliation() {
-        return affiliation;
-    }
-    
-    /**
-     * Sets the affiliation.
-     *
-     * @param the
-     *            affiliation
-     */
-    public void setAffiliation(Affiliation affiliation) {
-        this.affiliation = affiliation;
-    }
-    
-    /**
-     * Gets the Interests.
-     *
-     * @return the Interests
-     */
-    public Interests getInterests() {
-        return interests;
-    }
-    
-    /**
-     * Sets the Interests.
-     *
-     * @param the
-     *            Interests
-     */
-    public void setInterests(Interests interests) {
-        this.interests = interests;
-    }
-    
+	
+	/** The profileMeterMessage. */
+	private String profileMeterMessage;
+	
+	/** The dashBoardInfo. */
+	private List<DashBoardInfo> dashBoardInfo;
+	/**
+	 * Gets the profileMeterMessage.
+	 *
+	 * @return the profileMeterMessage
+	 * 
+	 */
+	public String getProfileMeterMessage() {
+		return profileMeterMessage;
+	}
+	/**
+	 * Sets the profileMeterMessage.
+	 *
+	 * @param the
+	 *            profileMeterMessage
+	 * 
+	 */
+	public void setProfileMeterMessage(String profileMeterMessage) {
+		this.profileMeterMessage = profileMeterMessage;
+	}
+	
+	/**
+	 * Gets the DashBoardInfo List.
+	 *
+	 * @return the
+	 *            DashBoardInfo List
+	 * 
+	 */
+	public List<DashBoardInfo> getDashBoardInfo() {
+		return dashBoardInfo;
+	}
+	/**
+	 * Sets the DashBoardInfo List.
+	 *
+	 * @param the
+	 *            DashBoardInfo List
+	 * 
+	 */
+	public void setDashBoardInfo(List<DashBoardInfo> dashBoardInfo) {
+		this.dashBoardInfo = dashBoardInfo;
+	}
 }
