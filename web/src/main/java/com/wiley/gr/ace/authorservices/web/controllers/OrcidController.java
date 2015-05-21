c0f602b54df13775c93304d984c9b80cc26ebb90
Dishari <dide@wiley.com>
2015-05-21 12:31:56 +0530

    AS-19 | dide | fetching orcid details from properties

--- web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/OrcidController.java
+++ web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/OrcidController.java
@@ -17,6 +17,7 @@ package com.wiley.gr.ace.authorservices.web.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,84 +38,98 @@ import com.wiley.gr.ace.authorservices.services.service.OrcidService;
 @RestController
 @RequestMapping("user/orcid")
 public class OrcidController {
-    
-    private static final Logger LOGGER = LoggerFactory
-            .getLogger(OrcidController.class);
-    
-    @Autowired(required = true)
-    OrcidService orcidService;
-    
-    /**
-     * @return ORCID URL
-     */
-    @RequestMapping(value = "/url", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody Service getOrcidURL() {
-        Service service = new Service();
-        
-        try {
-            /**
-             * Depending on the environment the ORCID URL changes
-             */
-            String env = "DEV";// Need to fetch this from prop file
-            String url = "";
-            if (null != env) {
-                url = "https://sandbox.orcid.org/oauth/authorize?client_id=APP-EU26CF7K2HTG34SV&response_type=code&scope=/authenticate&redirect_uri=http://www.vinay.com:8080/orcid/index.jsp";
-                service.setStatus("SUCCESS");
-                service.setPayload(url);
-            }
-        } catch (Exception e) {
-            ErrorPOJO error = new ErrorPOJO();
-            error.setCode(-101); // Need to set proper error code this one is
-                                 // dummy
-            error.setMessage("Error while fetching ORCID URL");
-            
-            service.setStatus("error");
-            service.setError(error);
-            throw new ASException("-2", "Error while fetching ORCID URL", e);
-        }
-        return service;
-    }
-    
-    /**
-     * @param authorizationCode
-     * @return
-     */
-    @RequestMapping(value = "/profile/{type}/{authorizationCode}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody Service getOrcidDetails(@PathVariable String type,
-            @PathVariable String authorizationCode) {
-        Service service = new Service();
-        User user = null;
-        try {
-            
-            if (null != authorizationCode) {
-                OrcidAccessToken accessToken = orcidService
-                        .getAccessToken(authorizationCode);
-                if (null != accessToken) {
-                    System.out.println("accessToken.getAccess_token() --->"
-                            + accessToken.getAccess_token());
-                    System.out.println("accessToken.getOrcid() ---> "
-                            + accessToken.getOrcid());
-                    if (null != type) {
-                        user = orcidService.getBio(accessToken);
-                        if (type.equalsIgnoreCase("userupdate")) {
-                            orcidService.getWork(user);
-                        }
-                    }
-                    service.setStatus("SUCCESS");
-                    service.setPayload(user);
-                }
-            }
-        } catch (Exception e) {
-            LOGGER.error("Stack Trace-.", e);
-            ErrorPOJO error = new ErrorPOJO();
-            error.setCode(-101); // Need to set proper error code this one is
-                                 // dummy
-            error.setMessage("Error while fetching ORCID details");
-            
-            service.setStatus("error");
-            service.setError(error);
-            throw new ASException("-2", "Error while fetching ORCID details", e);
-        }
-        return service;
-    }
+
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(OrcidController.class);
+
+	@Autowired(required = true)
+	OrcidService orcidService;
+
+	@Value("${orcid.url}")
+	private String orcidUrl;
+
+	@Value("${orcid-clientid}")
+	private String orcidClientId;
+
+	@Value("${orcid-redirect.url}")
+	private String orcidRedirectUrl;
+
+	/**
+	 * @return ORCID URL
+	 */
+	@RequestMapping(value = "/url", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Service getOrcidURL() {
+		Service service = new Service();
+
+		try {
+			/**
+			 * Depending on the environment the ORCID URL changes
+			 */
+			String env = "DEV";// Need to fetch this from prop file
+			String url = "";
+			if (null != env) {
+				url = "https://"
+						+ orcidUrl
+						+ "/oauth/authorize?client_id="
+						+ orcidClientId
+						+ "&response_type=code&scope=/authenticate&redirect_uri="
+						+ orcidRedirectUrl;
+				service.setStatus("SUCCESS");
+				service.setPayload(url);
+			}
+		} catch (Exception e) {
+			ErrorPOJO error = new ErrorPOJO();
+			error.setCode(-101); // Need to set proper error code this one is
+									// dummy
+			error.setMessage("Error while fetching ORCID URL");
+
+			service.setStatus("error");
+			service.setError(error);
+			throw new ASException("-2", "Error while fetching ORCID URL", e);
+		}
+		return service;
+	}
+
+	/**
+	 * @param authorizationCode
+	 * @return
+	 */
+	@RequestMapping(value = "/profile/{type}/{authorizationCode}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Service getOrcidDetails(@PathVariable String type,
+			@PathVariable String authorizationCode) {
+		Service service = new Service();
+		User user = null;
+		try {
+
+			if (null != authorizationCode) {
+				OrcidAccessToken accessToken = orcidService
+						.getAccessToken(authorizationCode);
+				if (null != accessToken) {
+					System.out.println("accessToken.getAccess_token() --->"
+							+ accessToken.getAccess_token());
+					System.out.println("accessToken.getOrcid() ---> "
+							+ accessToken.getOrcid());
+					if (null != type) {
+						user = orcidService.getBio(accessToken);
+						if (type.equalsIgnoreCase("userupdate")) {
+							orcidService.getWork(user);
+						}
+					}
+					service.setStatus("SUCCESS");
+					service.setPayload(user);
+				}
+			}
+		} catch (Exception e) {
+			LOGGER.error("Stack Trace-.", e);
+			ErrorPOJO error = new ErrorPOJO();
+			error.setCode(-101); // Need to set proper error code this one is
+									// dummy
+			error.setMessage("Error while fetching ORCID details");
+
+			service.setStatus("error");
+			service.setError(error);
+			throw new ASException("-2", "Error while fetching ORCID details", e);
+		}
+		return service;
+	}
 }
