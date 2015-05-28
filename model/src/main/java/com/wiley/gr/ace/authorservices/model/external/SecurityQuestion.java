5116288a56648e2c1bd435b68b0b6d22bab8006d
yugandhar <ykandula@wiley.com>
2015-05-28 12:41:23 +0530

    AS2-21|yugandhark|Added SecurityQuestions.java,SecuirtyQuestionDetails.java,SecurityQuestion.java classes

---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/external/SecurityQuestion.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
+ *
+ * All material contained herein is proprietary to John Wiley & Sons 
+ * and its third party suppliers, if any. The methods, techniques and 
+ * technical concepts contained herein are considered trade secrets 
+ * and confidential and may be protected by intellectual property laws.  
+ * Reproduction or distribution of this material, in whole or in part, 
+ * is strictly forbidden except by express prior written permission 
+ * of John Wiley & Sons.
+ *******************************************************************************/
+/**
+ * 
+ */
+package com.wiley.gr.ace.authorservices.model.external;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+/**
+ * @author yugandhark
+ *
+ */
+@JsonInclude(Include.NON_NULL)
+public class SecurityQuestion {
+    private String question;
+    private String answer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+}
