6061ee0ffb479c195bdec266bbc22033f62a67bc
Sarma <skpalli@wiley.com>
2015-05-29 21:37:43 +0530

    Create Admin User

--- model/src/main/java/com/wiley/gr/ace/authorservices/model/AdminUser.java
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/AdminUser.java
@@ -1,13 +1,21 @@
 package com.wiley.gr.ace.authorservices.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 /**
  * @author SarmaKumarap
  */
+@JsonInclude(Include.NON_NULL)
 public class AdminUser {
     
     private String emailId;
     
     private String roleId;
+    
+    private String firstName;
+    
+    private String lastName;
     
     /**
      * @return
@@ -35,6 +43,34 @@ public class AdminUser {
      */
     public void setRoleId(String roleId) {
         this.roleId = roleId;
+    }
+
+    /**
+     * @return
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
     
 }
