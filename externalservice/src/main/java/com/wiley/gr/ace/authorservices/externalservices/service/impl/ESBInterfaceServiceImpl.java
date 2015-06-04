a59759191f85ffb8c86f8bd538638fdeec930db8
Dishari <dide@wiley.com>
2015-06-04 17:11:22 +0530

    AS2-18 | dide | modified create user code

--- externalservice/src/main/java/com/wiley/gr/ace/authorservices/externalservices/service/impl/ESBInterfaceServiceImpl.java
+++ externalservice/src/main/java/com/wiley/gr/ace/authorservices/externalservices/service/impl/ESBInterfaceServiceImpl.java
@@ -20,6 +20,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
@@ -159,7 +160,12 @@ public class ESBInterfaceServiceImpl implements ESBInterfaceService {
 				profileForCreation, requestHeaders);
 		ResponseEntity<Status> response = restTemplate.exchange(uri,
 				HttpMethod.POST, requestEntity, Status.class);
-		status = response.getBody();
+		HttpStatus httpStatus = response.getStatusCode();
+		if(httpStatus.equals(HttpStatus.OK)) {
+			status.setStatus("SUCCESS");
+		} else {
+			status.setStatus("FAILURE");
+		}
 		return status;
 	}
 
