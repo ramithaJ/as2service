53d35d8467cda222582a6e14ee61607297872145
ravi <rasinha@wiley.com>
2015-06-10 17:09:54 +0530

    Era Issue Fixed

--- web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/AdminLoginController.java
+++ web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/AdminLoginController.java
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,102 +32,129 @@ import com.wiley.gr.ace.authorservices.model.UserManagement;
 import com.wiley.gr.ace.authorservices.services.service.AdminLoginService;
 
 /**
- * This method takes email in encrypted format like -- kondavinay%40gmail.com/
- * Hit the link as
- * http://localhost:8080/author-services/admin/login/kondavinay%40gmail.com/
+ * This method takes email in encrypted format like 
  * 
- * @author virtusa
- *	version 1.0
+ */
+/**
+ * @author virtusa version 1.0
  */
 @RestController
 @RequestMapping("/admin")
 public class AdminLoginController extends ASExceptionController {
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(AdminLoginController.class);
+   
+    /**
+     * Logger Configured.
+     */
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(AdminLoginController.class);
+   /** Error code From Props File. */
+    @Value("${adminnotexist.code}")
+    private String errorcode;
+    /** Error message From Props File. */
+    @Value("${adminnotexist.message}")
+    private String errormessage;
+    /** getting bean of adminlogin service. */
+    @Autowired(required = true)
+    private AdminLoginService adminLoginService;
 
-	@Value("${adminnotexist.code}")
-	public String errorcode;
-	@Value("${adminnotexist.message}")
-	public String errormessage;
-	@Autowired(required = true)
-	AdminLoginService adminLoginService;
-	@Autowired(required = true)
-	LocalValidatorFactoryBean validator;
+ 
+    /**
+     * @param request
+     * @return service
+     */
+    @RequestMapping(value = "/login/", method = RequestMethod.POST)
+    public final Service login(final HttpServletRequest request) {
+        LOGGER.info("Inside Login Method");
+        String emailId = (String) request.getAttribute("emailId");
+        Service service = new Service();
+        if (adminLoginService.validateEmail(emailId)) {
 
-	@RequestMapping(value = "/login/", method = RequestMethod.POST)
-	public Service login(HttpServletRequest request) {
-		LOGGER.info("Inside Login Method");
-		String emailId = (String) request.getAttribute("emailId");
-		Service service = new Service();
-		if (adminLoginService.validateEmail(emailId)) {
+            String userId = adminLoginService.doLogin(emailId);
+            LOGGER.debug(userId + "Geeting User id from dologin");
+            UserManagement userObj = new UserManagement();
+            userObj.setUserId(userId);
+            service.setPayload(userObj);
+        } else {
+            throw new ASException(errorcode, errormessage);
+        }
+        return service;
+    }
 
-			String userId = adminLoginService.doLogin(emailId);
-			LOGGER.debug(userId + "Geeting User id from dologin");
-			UserManagement userObj = new UserManagement();
-			userObj.setUserId(userId);
-			service.setPayload(userObj);
-		} else {
-			throw new ASException(errorcode, errormessage);
-		}
-		return service;
-	}
+    /**
+     * @param emailId
+     * @param accessId
+     * @return service
+     */
+    @RequestMapping(value = "/requestAccess/{emailId}/{accessId}/", method = RequestMethod.POST)
+    public final Service requestAccess(
+            @PathVariable("emailId") final String emailId,
+            @PathVariable("accessId") final String accessId) {
+        LOGGER.info("inside requestAccess Method");
+        return new Service();
 
-	@RequestMapping(value = "/requestAccess/{emailId}/{accessId}/", method = RequestMethod.POST)
-	public Service requestAccess(@PathVariable("emailId") String emailId,
-			@PathVariable("accessId") String accessId) {
-		LOGGER.info("inside requestAccess Method");
-		return new Service();
+    }
 
-	}
+    /**
+     * @param admin
+     * @return service
+     */
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public final Service createAdmin(@RequestBody final AdminUser admin) {
+        LOGGER.info("inside create Admin");
+        adminLoginService.createAdmin(admin);
+        return new Service();
 
-	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	public Service createAdmin(@RequestBody AdminUser admin) {
-		LOGGER.info("inside create Admin");
-		adminLoginService.createAdmin(admin);
-		return new Service();
+    }
 
-	}
+    
+    /**
+     * @return service
+     */
+    @RequestMapping(value = "/permissions/", method = RequestMethod.GET)
+    public final Service getPermissions() {
+        LOGGER.info("Inside Get Permission");
+        Service service = new Service();
+        service.setPayload(adminLoginService.getRolesAndPermissions(null));
+        return service;
+    }
 
-	@RequestMapping(value = "/permissions/", method = RequestMethod.GET)
-	public Service getPermissions() {
-		LOGGER.info("Inside Get Permission");
-		Service service = new Service();
-		service.setPayload(adminLoginService.getRolesAndPermissions(null));
-		return service;
-	}
+    /**
+     * @param roleId
+     * @return service
+     */
+    @RequestMapping(value = "/permissions/{roleId}", method = RequestMethod.GET)
+    public final Service getPermissionsForRole(
+            @PathVariable("roleId") final String roleId) {
+        LOGGER.info("Inside Get getPermissionsForRole");
+        Service service = new Service();
+        service.setPayload(adminLoginService.getRolesAndPermissions(roleId));
+        return service;
+    }
 
-	/**
-	 * @param roleId
-	 * @return
-	 */
-	@RequestMapping(value = "/permissions/{roleId}", method = RequestMethod.GET)
-	public Service getPermissionsForRole(@PathVariable("roleId") String roleId) {
-		LOGGER.info("Inside Get getPermissionsForRole");
-		Service service = new Service();
-		service.setPayload(adminLoginService.getRolesAndPermissions(roleId));
-		return service;
-	}
+    /**
+     * @param rolesAndPermissions
+     * @return service
+     */
+    @RequestMapping(value = "/permissions/", method = RequestMethod.POST)
+    public final Service addOrUpdateUserRole(
+            @RequestBody final ASRolesAndPermissions rolesAndPermissions) {
+        LOGGER.info("Inside Get addOrUpdateUserRole");
+        adminLoginService.addOrUpdateUserRole(rolesAndPermissions);
+        return new Service();
 
-	/**
-	 * @param rolesAndPermissions
-	 * @return
-	 */
-	@RequestMapping(value = "/permissions/", method = RequestMethod.POST)
-	public Service addOrUpdateUserRole(
-			@RequestBody ASRolesAndPermissions rolesAndPermissions) {
-		LOGGER.info("Inside Get addOrUpdateUserRole");
-		adminLoginService.addOrUpdateUserRole(rolesAndPermissions);
-		return new Service();
+    }
 
-	}
+    /**
+     * @param emailId
+     * @return service
+     */
+    @RequestMapping(value = "/findUser/{emailId}/", method = RequestMethod.GET)
+    public final Service findUser(@PathVariable("emailId") final String emailId) {
+        LOGGER.info("Inside Get findUser");
+        Service service = new Service();
+        // check if user exists, if yes return that data
+        service.setPayload(adminLoginService.findUser(emailId));
+        return service;
 
-	@RequestMapping(value = "/findUser/{emailId}/", method = RequestMethod.GET)
-	public Service findUser(@PathVariable("emailId") String emailId) {
-		LOGGER.info("Inside Get findUser");
-		Service service = new Service();
-		// check if user exists, if yes return that data
-		service.setPayload(adminLoginService.findUser(emailId));
-		return service;
-
-	}
+    }
 }
