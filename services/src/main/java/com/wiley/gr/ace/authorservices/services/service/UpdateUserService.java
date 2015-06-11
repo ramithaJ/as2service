6ed27f4b41f9361496d9593375116b5112c00026
yugandhar <ykandula@wiley.com>
2015-06-11 16:49:13 +0530

    As2-19|ykandula|updateOrcidDetails() method was removed

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/service/UpdateUserService.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/UpdateUserService.java
@@ -14,14 +14,11 @@
  */
 package com.wiley.gr.ace.authorservices.services.service;
 
-import com.wiley.gr.ace.authorservices.model.User;
 
 /**
  * @author virtusa version 1.0
  */
 public interface UpdateUserService {
-
-    User updateOrcidProfile(String orcidId, String userId) throws Exception;
 
     boolean updateOrcidId(String emailId, String orcidId, String userId)
             throws Exception;
--- services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/UpdateUserServiceImpl.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/UpdateUserServiceImpl.java
@@ -20,11 +20,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 
 import com.wiley.gr.ace.authorservices.externalservices.service.UserProfiles;
-import com.wiley.gr.ace.authorservices.externalservices.service.ESBInterfaceService;
 import com.wiley.gr.ace.authorservices.model.User;
 import com.wiley.gr.ace.authorservices.model.UserProfile;
 import com.wiley.gr.ace.authorservices.model.external.UserProfileResponse;
-import com.wiley.gr.ace.authorservices.persistence.services.UpdateUserDAO;
 import com.wiley.gr.ace.authorservices.services.service.UpdateUserService;
 
 /**
@@ -35,56 +33,13 @@ public class UpdateUserServiceImpl implements UpdateUserService {
     private static final Logger LOGGER = LoggerFactory
             .getLogger(UpdateUserServiceImpl.class);
 
-    @Autowired(required = true)
-    ESBInterfaceService esbInterfaceService;
     @Autowired
     UserProfiles userProfileService;
-    @Autowired(required = true)
-    UpdateUserDAO userDao;
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.wiley.gr.ace.authorservices.services.service.UpdateUserService#
-     * updateOrcidProfile(java.lang.String)
-     */
-    @Override
-    public User updateOrcidProfile(String orcidId, String userId)
-            throws Exception {
-
-        /**
-         * Fetch Account details and Profile details from external service
-         * (ESB->ORCID)
-         */
-        User user = esbInterfaceService.fetchOrcidDetails(orcidId);
-        User updatedUser = null;
-        if (user != null) {
-            /**
-             * Code to update ALM user attributes through ESB
-             */
-            String status = esbInterfaceService.updateALMUser(user);
-            LOGGER.debug("ALM user update status :: " + status);
-
-            if (null != status && "success".equalsIgnoreCase(status)) {
-                /**
-                 * Update the user account details with ORCID account details
-                 */
-                user.setUserId(Integer.valueOf(userId));
-                updatedUser = userDao.updateUserWithOrcid(user);
-            }
-        }
-        return updatedUser;
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see com.wiley.gr.ace.authorservices.services.service.UpdateUserService#
-     * updateOrcidId(java.lang.String,java.lang.String, java.lang.String)
-     */
     @Override
     public boolean updateOrcidId(String emailId, String orcidId, String userId)
             throws Exception {
+        LOGGER.info("inside updateOrcidId method of UpdateUserServiceImpl");
         boolean result = false;
         UserProfileResponse userProfileResponse = userProfileService
                 .userProfileResponse(userId);
