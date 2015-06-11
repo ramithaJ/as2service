e834a2a693217f686ed22d5bc82e42396d7c09ff
yugandhar <ykandula@wiley.com>
2015-06-11 16:50:08 +0530

    As2-19|ykandula|updateOrcidDetails() method was removed

--- web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/UpdateUserController.java
+++ web/src/main/java/com/wiley/gr/ace/authorservices/web/controllers/UpdateUserController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.wiley.gr.ace.authorservices.exception.ASException;
 import com.wiley.gr.ace.authorservices.model.ErrorPOJO;
 import com.wiley.gr.ace.authorservices.model.Service;
-import com.wiley.gr.ace.authorservices.model.User;
 import com.wiley.gr.ace.authorservices.services.service.UpdateUserService;
 
 /**
@@ -37,41 +36,6 @@ public class UpdateUserController {
 
     @Autowired(required = true)
     UpdateUserService updateUserService;
-
-    /**
-     * @param orcidId
-     * @param userId
-     * @return
-     */
-    @RequestMapping(value = "/{orcidId}/{userId}", method = RequestMethod.POST, produces = "application/json")
-    public @ResponseBody Service updateUserOrcidDetails(
-            @PathVariable String orcidId, @PathVariable String userId) {
-
-        User user = null;
-        Service service = new Service();
-        try {
-            user = updateUserService.updateOrcidProfile(orcidId, userId);
-            if (user != null) {
-                service.setStatus("success");
-                service.setPayload(user);
-            } else {
-                service.setStatus("failure");
-                service.setPayload(user);
-            }
-        } catch (Exception e) {
-            ErrorPOJO error = new ErrorPOJO();
-            error.setCode(-100); // Need to set proper error code this one is
-                                 // dummy
-            error.setMessage("Error updating user details with ORCID details");
-
-            service.setStatus("error");
-            service.setPayload(user);
-            service.setError(error);
-            throw new ASException("-1",
-                    "Error updating user details with ORCID details", e);
-        }
-        return service;
-    }
 
     /**
      * @param orcidId
