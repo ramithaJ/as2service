7d59c5f5e20851b65880e977565991919d1047a8
yugandhar <ykandula@wiley.com>
2015-06-15 18:28:03 +0530

    AS2-117|ykandula|Code is added for associationConfirmation

---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/external/AssociationConfirmation.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.wiley.gr.ace.authorservices.model.external;
+
+/**
+ * @author yugandhark
+ *
+ */
+public class AssociationConfirmation {
+
+	private Integer articleAuthId;
+
+	private Integer userId;
+
+	public Integer getArticleAuthId() {
+		return articleAuthId;
+	}
+
+	public void setArticleAuthId(Integer articleAuthId) {
+		this.articleAuthId = articleAuthId;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+}
