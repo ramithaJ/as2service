1189ed0148f1f8b3b1f19714e5377532e60567be
yugandhar <ykandula@wiley.com>
2015-06-17 17:06:43 +0530

    AS2-124|ykandula|Code added for Viewing the article data

---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/external/License.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.wiley.gr.ace.authorservices.model.external;
+
+/**
+ * @author yugandhark
+ *
+ */
+public class License {
+	private String licenseStatus;
+
+	public String getLicenseStatus() {
+		return licenseStatus;
+	}
+
+	public void setLicenseStatus(String licenseStatus) {
+		this.licenseStatus = licenseStatus;
+	}
+	
+}
---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/external/OnlineOpen.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.wiley.gr.ace.authorservices.model.external;
+
+/**
+ * @author yugandhark
+ *
+ */
+public class OnlineOpen {
+	private String onlineOpenStatus;
+
+	public String getOnlineOpenStatus() {
+		return onlineOpenStatus;
+	}
+
+	public void setOnlineOpenStatus(String onlineOpenStatus) {
+		this.onlineOpenStatus = onlineOpenStatus;
+	}
+	
+}
