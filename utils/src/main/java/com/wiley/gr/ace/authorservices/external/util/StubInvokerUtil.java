5a9b84aa7b7704f93fa2fb984aead7a78e3048f5
Shiva <skpunna@wiley.com>
2015-06-18 11:44:02 +0530

    AS2-1 | skpunna | PMD fix

--- utils/src/main/java/com/wiley/gr/ace/authorservices/external/util/StubInvokerUtil.java
+++ utils/src/main/java/com/wiley/gr/ace/authorservices/external/util/StubInvokerUtil.java
@@ -3,7 +3,6 @@ package com.wiley.gr.ace.authorservices.external.util;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
