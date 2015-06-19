6d0d561cc3a61f602a8ec47c7df0809c42c1c9d5
Kalyan Jeedigunta <kcjeedigun@wiley.com>
2015-06-19 18:12:29 +0530

    18397 | kcjeedigun | Author Paid Online Open

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/OrderOnlineOpenServiceImpl.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/OrderOnlineOpenServiceImpl.java
@@ -19,8 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.wiley.gr.ace.authorservices.exception.ASException;
 import com.wiley.gr.ace.authorservices.externalservices.service.OrderService;
-import com.wiley.gr.ace.authorservices.model.Address;
-import com.wiley.gr.ace.authorservices.model.AddressDetails;
 import com.wiley.gr.ace.authorservices.model.Amount;
 import com.wiley.gr.ace.authorservices.model.Discounts;
 import com.wiley.gr.ace.authorservices.model.FunderDetails;
@@ -41,109 +39,123 @@ import com.wiley.gr.ace.authorservices.services.service.OrderOnlineOpenService;
  */
 public class OrderOnlineOpenServiceImpl implements OrderOnlineOpenService {
 
-    /** Getting Bean Of Order Service */
-    @Autowired(required = true)
-    private OrderService orderservice;
-    
-    /** Getting Bean Of OrderOnlineDAO Service */
-    @Autowired(required = true)
-    private OrderOnlineDAO orderOnlineDAO;
+	/** Getting Bean Of Order Service */
+	@Autowired(required = true)
+	private OrderService orderservice;
 
-    /**
-     * This method will take userId and orderId as input and calls external
-     * service to get details of Online order details
-     */
-    @Override
-    public OnlineOpenOrder getOnlineOpenOrderDetails(final String userId,
-            final String orderId) {
-        OnlineOpenOrder onlineOpenOrder = new OnlineOpenOrder();
-        OrderData orderData = orderservice.getOrderDetails(userId, orderId);
-        onlineOpenOrder.setArticleId(orderData.getArticleId());
-        onlineOpenOrder.setArticleId(orderData.getArticleId());
-        onlineOpenOrder.setJournalId(orderData.getJournalId());
-        onlineOpenOrder.setPaymentMethod(orderData.getPaymentMethod());
-        onlineOpenOrder.setTaxPercentage(orderData.getTaxPercentage());
-        Discounts discounts = new Discounts();
-        discounts.setInstitutionId(orderData.getDiscountData()
-                .getInstitutionId());
-        discounts.setOtherDiscounts(orderData.getDiscountData()
-                .getOtherDiscounts());
-        discounts.setPromoCode(orderData.getDiscountData().getPromoCode());
-        discounts.setSocietyId(orderData.getDiscountData().getSocietyId());
-        onlineOpenOrder.setDiscounts(discounts);
-        Amount amount = new Amount();
-        amount.setAmount(orderData.getAmountPayable().getAmount());
-        amount.setAmount(orderData.getAmountPayable().getCurrency());
-        onlineOpenOrder.setAmountPayable(amount);
-        FunderDetails funderDetails = new FunderDetails();
-        List<FunderDetails> listoffunderdetails = new ArrayList<FunderDetails>();
-        funderDetails.setFunderId(orderData.getFundingDetails().getFunderId());
-        funderDetails.setResearchFunderId(orderData.getFundingDetails()
-                .getResearchFunderId());
-        funderDetails.setWoaAccountId(orderData.getFundingDetails()
-                .getWoaAccountId());
-        listoffunderdetails.add(funderDetails);
-        onlineOpenOrder.setFunderDetails(listoffunderdetails);
-        TaxDetails taxDetails = new TaxDetails();
-        taxDetails.setCountryCode(orderData.getTaxDetails().getCountryCode());
-        taxDetails.setTaxCodeExpiryDate(orderData.getTaxDetails()
-                .getTaxCodeExpiryDate());
-        taxDetails.setTaxExemptionNumber(orderData.getTaxDetails()
-                .getTaxExemptionNumber());
-        taxDetails.setVatExemptionNumber(orderData.getTaxDetails()
-                .getVatExemptionNumber());
-        taxDetails.setVatIdNumber(orderData.getTaxDetails().getVatIdNumber());
-        onlineOpenOrder.setTaxDetails(taxDetails);
-        Address address = new Address();
-        address.setAddressLine1(orderData.getAddresses().get(0)
-                .getAddressLine1());
-        AddressDetails addressDetails = new AddressDetails();
-        addressDetails.setAddress(null);
+	/** Getting Bean Of OrderOnlineDAO Service */
+	@Autowired(required = true)
+	private OrderOnlineDAO orderOnlineDAO;
 
-        return onlineOpenOrder;
-    }
+	/**
+	 * This method will take userId and orderId as input and calls external
+	 * service to get details of Online order details
+	 */
+	@Override
+	public OnlineOpenOrder getOnlineOpenOrderDetails(final String userId,
+			final String orderId) {
+		OnlineOpenOrder onlineOpenOrder = new OnlineOpenOrder();
+		OrderData orderData = orderservice.getOrderDetails(userId, orderId);
+		onlineOpenOrder.setArticleId(orderData.getArticle().getDHID());
+		// Should check the below one
+		// onlineOpenOrder.setJournalId(orderData.getArticle().getJournal().get);
+		onlineOpenOrder.setPaymentMethod(orderData.getPayment()
+				.getPaymentMethod());
+		// need to calculate this
+		// onlineOpenOrder.setTaxPercentage(orderData.);
+		Discounts discounts = new Discounts();
+		discounts.setInstitutionId(orderData.getPricing().getDiscounts()
+				.getDiscount().getAffiliationData().getName());
+		// Need to check this
+		// discounts.setOtherDiscounts(orderData.getDiscountData().getOtherDiscounts());
+		discounts.setPromoCode(orderData.getPricing().getDiscounts()
+				.getDiscount().getPromoCode());
+		discounts.setSocietyId(orderData.getPricing().getDiscounts()
+				.getDiscount().getSocietyData().getName());
+		onlineOpenOrder.setDiscounts(discounts);
+		Amount amount = new Amount();
+		amount.setAmount(orderData.getPricing().getAmountToBePaid().toString());
+		amount.setAmount(orderData.getPricing().getCurrency());
+		onlineOpenOrder.setAmountPayable(amount);
+		FunderDetails funderDetails = new FunderDetails();
+		List<FunderDetails> listoffunderdetails = new ArrayList<FunderDetails>();
+		// Currently not getting this data from stub
+		// funderDetails.setFunderId(orderData.getFundingDetails().getFunderId());
+		// funderDetails.setResearchFunderId(orderData.getFundingDetails()
+		// .getResearchFunderId());
+		funderDetails
+				.setWoaAccountId(orderData.getWoaAccountHolder().getName());
+		listoffunderdetails.add(funderDetails);
+		onlineOpenOrder.setFunderDetails(listoffunderdetails);
+		TaxDetails taxDetails = new TaxDetails();
+		taxDetails.setCountryCode(orderData.getTaxDetails().getCountryCode());
+		taxDetails.setTaxCodeExpiryDate(orderData.getTaxDetails()
+				.getTaxExpiration());
+		taxDetails.setTaxExemptionNumber(orderData.getTaxDetails()
+				.getTaxNumber());
+		taxDetails.setVatExemptionNumber(orderData.getTaxDetails()
+				.getVatExemptionNumber());
+		taxDetails.setVatIdNumber(orderData.getTaxDetails().getVatIdNumber());
+		onlineOpenOrder.setTaxDetails(taxDetails);
+		// TODO: Update Address details
+		// Address address = new Address();
+		// address.setAddressLine1(orderData.getAddresses().get(0)
+		// .getAddressLine1());
+		// AddressDetails addressDetails = new AddressDetails();
+		// addressDetails.setAddress(null);
 
-    /**
-     * Method to get quote.
-     */
-    @Override
-    public boolean getQuote(String userId, String articleId) {
+		return onlineOpenOrder;
+	}
 
-        // Article details having userId and articleId
-        Articles articles = orderOnlineDAO.getArticleDetails(articleId);
-        if(null == articles){
-            throw new ASException("801", "");
-        }
-        
-        PdhLookup pdhLookup = orderservice.pdhLookUp(articles.getJournals().getDhId());
-        // check article is onlineOpen article or not.
-        if(pdhLookup.getPdmSalesModel().equalsIgnoreCase("OO")){
-            
-            // Article Author Assignment table details having userId and articleId.
-            ArticleAuthorAssignment articleAuthorAssignment = orderOnlineDAO.getAritcleAssignmentDetails(userId, articleId);
-            if(articleAuthorAssignment == null) {
-                throw new ASException("802", "Article is not yet Accepted");
-            }
-            // check user is corresponding author or not.
-            if(articleAuthorAssignment.getArticleRoles().getArticleRoleCd().equalsIgnoreCase("0001")) {
-                
-                // check is there any saved orders for this article.
-                SavedOrders savedOrders = orderOnlineDAO.getSavedOrders(articleId, userId);
-                if(null != savedOrders) {
-                    throw new ASException("803", "Order already exists for this Article");
-                }
-                //check is there any placed orders for this article.
-                Orders orders = orderOnlineDAO.getOrder(articleAuthorAssignment.getArticleAuthId());
-                if(orders != null) {
-                    throw new ASException("804", "Order already submitted for this Article");
-                }
-                // calling external service for article and journal titles.
-                orderservice.pdhLookUpArticle(articles.getDhId());
-                orderservice.pdhLookUp(articles.getJournals().getDhId());                    
-            }
-        }
-        
-        return false;
-    }
-    
+	/**
+	 * Method to get quote.
+	 */
+	@Override
+	public boolean getQuote(String userId, String articleId) {
+
+		// Article details having userId and articleId
+		Articles articles = orderOnlineDAO.getArticleDetails(articleId);
+		if (null == articles) {
+			throw new ASException("801", "");
+		}
+
+		PdhLookup pdhLookup = orderservice.pdhLookUp(articles.getJournals()
+				.getDhId());
+		// check article is onlineOpen article or not.
+		if (pdhLookup.getPdmSalesModel().equalsIgnoreCase("OO")) {
+
+			// Article Author Assignment table details having userId and
+			// articleId.
+			ArticleAuthorAssignment articleAuthorAssignment = orderOnlineDAO
+					.getAritcleAssignmentDetails(userId, articleId);
+			if (articleAuthorAssignment == null) {
+				throw new ASException("802", "Article is not yet Accepted");
+			}
+			// check user is corresponding author or not.
+			if (articleAuthorAssignment.getArticleRoles().getArticleRoleCd()
+					.equalsIgnoreCase("0001")) {
+
+				// check is there any saved orders for this article.
+				SavedOrders savedOrders = orderOnlineDAO.getSavedOrders(
+						articleId, userId);
+				if (null != savedOrders) {
+					throw new ASException("803",
+							"Order already exists for this Article");
+				}
+				// check is there any placed orders for this article.
+				Orders orders = orderOnlineDAO.getOrder(articleAuthorAssignment
+						.getArticleAuthId());
+				if (orders != null) {
+					throw new ASException("804",
+							"Order already submitted for this Article");
+				}
+				// calling external service for article and journal titles.
+				orderservice.pdhLookUpArticle(articles.getDhId());
+				orderservice.pdhLookUp(articles.getJournals().getDhId());
+			}
+		}
+
+		return false;
+	}
+
 }
