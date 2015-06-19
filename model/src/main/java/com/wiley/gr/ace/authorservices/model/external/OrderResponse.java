69615abf4d0c8614f22ac0e553f3754a9c1e7a69
Kalyan Jeedigunta <kcjeedigun@wiley.com>
2015-06-19 19:55:10 +0530

    18397 | kcjeedigun | Author Paid Online Open

---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/external/OrderResponse.java
@@ -0,0 +1,63 @@
+package com.wiley.gr.ace.authorservices.model.external;
+
+/**
+ * @author virtusa version1.0
+ *
+ */
+public class OrderResponse {
+
+	private String responseCode;
+	private String responseDescription;
+	private String ooUniqueId;
+
+	/**
+	 * 
+	 * @return The responseCode
+	 */
+	public String getResponseCode() {
+		return responseCode;
+	}
+
+	/**
+	 * 
+	 * @param responseCode
+	 *            The responseCode
+	 */
+	public void setResponseCode(String responseCode) {
+		this.responseCode = responseCode;
+	}
+
+	/**
+	 * 
+	 * @return The responseDescription
+	 */
+	public String getResponseDescription() {
+		return responseDescription;
+	}
+
+	/**
+	 * 
+	 * @param responseDescription
+	 *            The responseDescription
+	 */
+	public void setResponseDescription(String responseDescription) {
+		this.responseDescription = responseDescription;
+	}
+
+	/**
+	 * 
+	 * @return The ooUniqueId
+	 */
+	public String getOoUniqueId() {
+		return ooUniqueId;
+	}
+
+	/**
+	 * 
+	 * @param ooUniqueId
+	 *            The ooUniqueId
+	 */
+	public void setOoUniqueId(String ooUniqueId) {
+		this.ooUniqueId = ooUniqueId;
+	}
+}
