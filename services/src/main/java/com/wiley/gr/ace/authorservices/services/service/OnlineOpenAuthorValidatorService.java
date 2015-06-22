6d82d053aea56f15dcab023b6019c11c346d0f7d
Kalyan Jeedigunta <kcjeedigun@wiley.com>
2015-06-22 14:51:04 +0530

    18397 | kcjeedigun | Author Paid Online Open

---
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/OnlineOpenAuthorValidatorService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.wiley.gr.ace.authorservices.services.service;
+
+/**
+ * @author virtusa version 1.0
+ *
+ */
+public interface OnlineOpenAuthorValidatorService {
+	
+	
+
+}
