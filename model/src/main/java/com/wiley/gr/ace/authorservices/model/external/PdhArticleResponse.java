64d12de2e8626233a26c4688190065f358917bc9
Kalyan Jeedigunta <kcjeedigun@wiley.com>
2015-06-22 20:06:23 +0530

    18397 | kcjeedigun | Author Paid Online Open

---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/external/PdhArticleResponse.java
@@ -0,0 +1,64 @@
+package com.wiley.gr.ace.authorservices.model.external;
+
+/**
+ * @author virtusa version1.0
+ *
+ */
+public class PdhArticleResponse {
+
+	private String articleId;
+	private String title;
+	private WOAFunders woaFunders;
+
+	/**
+	 * 
+	 * @return The articleId
+	 */
+	public String getArticleId() {
+		return articleId;
+	}
+
+	/**
+	 * 
+	 * @param articleId
+	 *            The articleId
+	 */
+	public void setArticleId(String articleId) {
+		this.articleId = articleId;
+	}
+
+	/**
+	 * 
+	 * @return The title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * 
+	 * @param title
+	 *            The title
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * 
+	 * @return The WOAFunders
+	 */
+	public WOAFunders getWOAFunders() {
+		return woaFunders;
+	}
+
+	/**
+	 * 
+	 * @param WOAFunders
+	 *            The WOAFunders
+	 */
+	public void setWOAFunders(WOAFunders woaFunders) {
+		this.woaFunders = woaFunders;
+	}
+
+}
