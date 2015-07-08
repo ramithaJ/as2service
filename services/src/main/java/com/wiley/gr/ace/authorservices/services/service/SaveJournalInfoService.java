9d0db87d423a3df9fab89688e7d3f976d499523f
yugandhar <ykandula@wiley.com>
2015-07-08 15:18:19 +0530

    AS2-127 | ykandula | Initial Classes for parsing and Saving Journal Info

---
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/SaveJournalInfoService.java
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
+ *
+ * All material contained herein is proprietary to John Wiley & Sons 
+ * and its third party suppliers, if any. The methods, techniques and 
+ * technical concepts contained herein are considered trade secrets 
+ * and confidential and may be protected by intellectual property laws.  
+ * Reproduction or distribution of this material, in whole or in part, 
+ * is strictly forbidden except by express prior written permission 
+ * of John Wiley & Sons.
+ *******************************************************************************/
+/**
+ * 
+ */
+package com.wiley.gr.ace.authorservices.services.service;
+
+/**
+ * The Interface SaveJournalInfoService.
+ *
+ * @author virtusa version 1.0
+ */
+public interface SaveJournalInfoService {
+
+    /**
+     * Parses the journal info type event.
+     *
+     * @param journalInfoTypeEvent
+     *            the journal info type event
+     * @throws Exception
+     *             the exception
+     */
+    void parseJournalInfoTypeEvent(String journalInfoTypeEvent)
+            throws Exception;
+}
