553d881544000838aaf48872af6331ff5dc357a7
Dishari <dide@wiley.com>
2015-07-10 15:39:56 +0530

    AS2-121 | dide | ERA

--- externalservice/src/main/java/com/wiley/gr/ace/authorservices/externalservices/service/ValidationService.java
+++ externalservice/src/main/java/com/wiley/gr/ace/authorservices/externalservices/service/ValidationService.java
@@ -1,9 +1,45 @@
+/*******************************************************************************
+ * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
+ *
+ * All material contained herein is proprietary to John Wiley & Sons 
+ * and its third party suppliers, if any. The methods, techniques and 
+ * technical concepts contained herein are considered trade secrets 
+ * and confidential and may be protected by intellectual property laws.  
+ * Reproduction or distribution of this material, in whole or in part, 
+ * is strictly forbidden except by express prior written permission 
+ * of John Wiley & Sons.
+ *******************************************************************************/
+/**
+ * 
+ */
 package com.wiley.gr.ace.authorservices.externalservices.service;
 
 import com.wiley.gr.ace.authorservices.model.external.AddressValidationRequest;
 
+/**
+ * The Interface ValidationService.
+ * 
+ * @author virtusa version 1.0
+ */
 public interface ValidationService {
 
-	boolean validateAddress(AddressValidationRequest addressValidationRequest) ;
-	boolean vatValidation(String countriCode, String taxVatRegNum);
+    /**
+     * Validate address.
+     *
+     * @param addressValidationRequest
+     *            the address validation request
+     * @return true, if successful
+     */
+    boolean validateAddress(AddressValidationRequest addressValidationRequest);
+
+    /**
+     * Vat validation.
+     *
+     * @param countryCode
+     *            the countri code
+     * @param taxVatRegNum
+     *            the tax vat reg num
+     * @return true, if successful
+     */
+    boolean vatValidation(String countryCode, String taxVatRegNum);
 }
--- externalservice/src/main/java/com/wiley/gr/ace/authorservices/externalservices/service/impl/ValidationServiceImpl.java
+++ externalservice/src/main/java/com/wiley/gr/ace/authorservices/externalservices/service/impl/ValidationServiceImpl.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
+ *
+ * All material contained herein is proprietary to John Wiley & Sons 
+ * and its third party suppliers, if any. The methods, techniques and 
+ * technical concepts contained herein are considered trade secrets 
+ * and confidential and may be protected by intellectual property laws.  
+ * Reproduction or distribution of this material, in whole or in part, 
+ * is strictly forbidden except by express prior written permission 
+ * of John Wiley & Sons.
+ *******************************************************************************/
+/**
+ * 
+ */
 package com.wiley.gr.ace.authorservices.externalservices.service.impl;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -12,49 +26,74 @@ import com.wiley.gr.ace.authorservices.model.external.AddressValidationRequest;
 import com.wiley.gr.ace.authorservices.model.external.AddressValidationResponse;
 import com.wiley.gr.ace.authorservices.model.external.VatIdValidationResponse;
 
+/**
+ * The Class ValidationServiceImpl.
+ * 
+ * @author virtusa version 1.0
+ */
 public class ValidationServiceImpl implements ValidationService {
 
-	@Value("$addressdoctor.url")
-	String addressDoctorUrl;
+    /** The address doctor url. */
+    @Value("$addressdoctor.url")
+    private String addressDoctorUrl;
 
-	@Value("$vatidvalidation.url")
-	String vatValidationUrl;
+    /** The vat validation url. */
+    @Value("$vatidvalidation.url")
+    private String vatValidationUrl;
 
-	@Override
-	public boolean validateAddress(
-			AddressValidationRequest addressValidationRequest) {
-		boolean isValid = false;
-		ObjectWriter ow = new ObjectMapper().writer()
-				.withDefaultPrettyPrinter();
-		try {
-			String addressRequestJson = ow
-					.writeValueAsString(addressValidationRequest);
-			String addressValidationUrl = addressDoctorUrl + addressRequestJson;
-			AddressValidationResponse addressValidationResponse = (AddressValidationResponse) StubInvokerUtil
-					.restGetServiceInvoker(addressValidationUrl,
-							AddressValidationResponse.class);
+    /**
+     * Validate address.
+     *
+     * @param addressValidationRequest
+     *            the address validation request
+     * @return true, if successful
+     */
+    @Override
+    public final boolean validateAddress(
+            final AddressValidationRequest addressValidationRequest) {
+        boolean isValid = false;
+        final ObjectWriter ow = new ObjectMapper().writer()
+                .withDefaultPrettyPrinter();
+        try {
+            final String addressRequestJson = ow
+                    .writeValueAsString(addressValidationRequest);
+            final String addressValidationUrl = addressDoctorUrl
+                    + addressRequestJson;
+            final AddressValidationResponse addressValidationResponse = (AddressValidationResponse) StubInvokerUtil
+                    .restGetServiceInvoker(addressValidationUrl,
+                            AddressValidationResponse.class);
 
-			if (StringUtils.isEmpty(addressValidationResponse)) {
-				isValid = true;
-			}
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
+            if (StringUtils.isEmpty(addressValidationResponse)) {
+                isValid = true;
+            }
+        } catch (final JsonProcessingException e) {
+            e.printStackTrace();
+        }
 
-		return isValid;
-	}
+        return isValid;
+    }
 
-	@Override
-	public boolean vatValidation(String countriCode, String taxVatRegNum) {
-		boolean isValid = false;
-		String vatUrl = vatValidationUrl + "countryCode=" + countriCode
-				+ "&taxRegistrationNumber=" + taxVatRegNum;
+    /**
+     * Vat validation.
+     *
+     * @param countryCode
+     *            the country code
+     * @param taxVatRegNum
+     *            the tax vat reg num
+     * @return true, if successful
+     */
+    @Override
+    public final boolean vatValidation(final String countryCode,
+            final String taxVatRegNum) {
+        boolean isValid = false;
+        final String vatUrl = vatValidationUrl + "countryCode=" + countryCode
+                + "&taxRegistrationNumber=" + taxVatRegNum;
 
-		VatIdValidationResponse vatIdValidationResponse = (VatIdValidationResponse) StubInvokerUtil
-				.restGetServiceInvoker(vatUrl, VatIdValidationResponse.class);
-		if ("true".equalsIgnoreCase(vatIdValidationResponse.getIsValid())) {
-			isValid = false;
-		}
-		return isValid;
-	}
+        final VatIdValidationResponse vatIdValidationResponse = (VatIdValidationResponse) StubInvokerUtil
+                .restGetServiceInvoker(vatUrl, VatIdValidationResponse.class);
+        if ("true".equalsIgnoreCase(vatIdValidationResponse.getIsValid())) {
+            isValid = false;
+        }
+        return isValid;
+    }
 }
