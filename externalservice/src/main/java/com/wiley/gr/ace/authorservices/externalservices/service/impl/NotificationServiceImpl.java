ea0a69f0e87c97f30e885bbc5fe9b5f36e29fe46
Dishari <dide@wiley.com>
2015-07-22 12:12:59 +0530

    AS2-39 | dide | notification services

---
+++ externalservice/src/main/java/com/wiley/gr/ace/authorservices/externalservices/service/impl/NotificationServiceImpl.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
+ *
+ * All material contained herein is proprietary to John Wiley & Sons 
+ * and its third party suppliers, if any. The methods, techniques and 
+ * technical concepts contained herein are considered trade secrets 
+ * and confidential and may be protected by intellectual property laws.  
+ * Reproduction or distribution of this material, in whole or in part, 
+ * is strictly forbidden except by express prior written permission 
+ * of John Wiley & Sons.
+ *******************************************************************************/
+
+package com.wiley.gr.ace.authorservices.externalservices.service.impl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+
+import com.wiley.gr.ace.authorservices.external.util.StubInvokerUtil;
+import com.wiley.gr.ace.authorservices.externalservices.service.NotificationService;
+import com.wiley.gr.ace.authorservices.model.NotificationHistory;
+import com.wiley.gr.ace.authorservices.model.NotificationResponse;
+import com.wiley.gr.ace.authorservices.model.external.NotificationRequest;
+
+/**
+ * The Class NotificationServiceImpl.
+ * 
+ * @author virtusa version 1.0
+ */
+public class NotificationServiceImpl implements NotificationService {
+
+    /** The Notification user. */
+    @Value("${notification.url}")
+    private String notificationurl;
+
+    /** The Notification History. */
+    @Value("${notificationHistory.url}")
+    private String notificationHistoryUrl;
+
+    /* (non-Javadoc)
+     * @see com.wiley.gr.ace.authorservices.externalservices.service.NotificationService#sendNotification(java.lang.String, java.lang.String, java.lang.String, com.wiley.gr.ace.authorservices.model.external.NotificationRequest)
+     */
+    @Override
+    public final NotificationResponse sendNotification(String appId,
+            String type, String templateId,
+            final NotificationRequest notificationRequest) {
+
+        String notificationFinalUrl = notificationurl + appId + "/send?tmpl="
+                + templateId + "&type=" + type;
+        return (NotificationResponse) StubInvokerUtil.restServiceInvoker(
+                notificationFinalUrl, notificationRequest,
+                NotificationResponse.class);
+    }
+
+    /**
+     * Gets the notification history.
+     *
+     * @param userId
+     *            the user id
+     * @return the notification history
+     * @throws Exception
+     *             the exception
+     */
+    @Override
+    public final NotificationHistory getNotificationHistory(final String userId)
+            throws Exception {
+        return (NotificationHistory) StubInvokerUtil.invokeStub(
+                notificationHistoryUrl, HttpMethod.GET,
+                NotificationHistory.class);
+    }
+}
