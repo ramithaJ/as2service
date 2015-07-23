4a590bd3e16f1b609afa985c729f27164e08d850
Sarma <skpalli@wiley.com>
2015-07-23 19:26:36 +0530

    Added get method to call ESB Services

--- utils/src/main/java/com/wiley/gr/ace/authorservices/external/util/RestServiceInvokerUtil.java
+++ utils/src/main/java/com/wiley/gr/ace/authorservices/external/util/RestServiceInvokerUtil.java
@@ -85,12 +85,34 @@ public class RestServiceInvokerUtil {
 
     }
 
-    // public static void main(String[] args) {
-    // Login login = new Login();
-    // login.setEmailId("ssaraf@wiley.com");
-    // login.setPassword("Sripad1234");
-    // RestServiceInvokerUtil.invokeService("http://10.201.64.81:8080/service/v1/auth/authenticate",
-    // HttpMethod.POST, "Login", login);
-    // }
+    /**
+     * Rest get service invoker.
+     *
+     * @param <T>
+     *            the generic type
+     * @param url
+     *            the url
+     * @param responseEntityClass
+     *            the response entity class
+     * @return the object
+     */
+    public static <T> Object getServiceData(final String url,
+            final Class<T> responseEntityClass) {
+
+        try {
+            ResponseEntity<T> response = new RestTemplate().getForEntity(
+                    new URI(url), responseEntityClass);
+
+            if (null == response) {
+                return new SecurityResponse();
+            }
+            return response.getBody();
+        } catch (URISyntaxException e) {
+
+            throw new ASException();
+
+        }
+
+    }
 
 }
