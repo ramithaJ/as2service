f6a24b070c1be7f4b26a2801b9c805425c9be89b
yugandhar <ykandula@wiley.com>
2015-07-27 19:39:00 +0530

    AS2-117 | ykandula : Article Assignment

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/ArticleAssignmentServiceImpl.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/impl/ArticleAssignmentServiceImpl.java
@@ -14,16 +14,29 @@
  */
 package com.wiley.gr.ace.authorservices.services.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 
 import com.wiley.gr.ace.authorservices.externalservices.service.ESBInterfaceService;
 import com.wiley.gr.ace.authorservices.externalservices.service.OrderService;
 import com.wiley.gr.ace.authorservices.externalservices.service.TaskService;
+import com.wiley.gr.ace.authorservices.model.ArticleDetails;
 import com.wiley.gr.ace.authorservices.model.AssociationConfirmation;
+import com.wiley.gr.ace.authorservices.model.JournalDetails;
+import com.wiley.gr.ace.authorservices.model.LicenseDetails;
+import com.wiley.gr.ace.authorservices.model.OrderDetails;
+import com.wiley.gr.ace.authorservices.model.PublicationDetails;
+import com.wiley.gr.ace.authorservices.model.ViewAssignedArticle;
+import com.wiley.gr.ace.authorservices.model.external.Art;
 import com.wiley.gr.ace.authorservices.model.external.ArticleInfoDetails;
-import com.wiley.gr.ace.authorservices.model.external.ViewAssignedArticle;
+import com.wiley.gr.ace.authorservices.model.external.CoAuthorDetails;
+import com.wiley.gr.ace.authorservices.model.external.GetArticleDetails;
+import com.wiley.gr.ace.authorservices.model.external.LastSignedLicense;
 import com.wiley.gr.ace.authorservices.services.service.ArticleAssignmentService;
 
 /**
@@ -96,7 +109,150 @@ public class ArticleAssignmentServiceImpl implements ArticleAssignmentService {
     public final ViewAssignedArticle viewAssignedArticle(final String emailId)
             throws Exception {
         LOGGER.info("inside viewAssignedArticle method of ArticleAssignmentServiceImpl");
-        return esbInterfaceService.viewAssignedArticle(emailId);
+        final ViewAssignedArticle viewAssignedArticle = new ViewAssignedArticle();
+        final GetArticleDetails getArticleDetails = esbInterfaceService
+                .viewAssignedArticle(emailId);
+        if (!StringUtils.isEmpty(getArticleDetails)) {
+            viewAssignedArticle
+                    .setArticleData(getArticleData(getArticleDetails));
+            viewAssignedArticle
+                    .setJournalData(getJournalData(getArticleDetails));
+            viewAssignedArticle
+                    .setPublicationData(getPublicationData(getArticleDetails));
+            viewAssignedArticle.setOrderData(getOrderId());
+            viewAssignedArticle
+                    .setLicenseData(getLicenseData(getArticleDetails));
+        }
+
+        return viewAssignedArticle;
+
+    }
+
+    /**
+     * Gets the article data.
+     *
+     * @param getArticleDetails
+     *            the get article details
+     * @return the article data
+     */
+    private final ArticleDetails getArticleData(
+            final GetArticleDetails getArticleDetails) {
+        ArticleDetails articleDetails = null;
+        final Art art = getArticleDetails.getArt();
+        if (!StringUtils.isEmpty(art)) {
+            articleDetails = new ArticleDetails();
+            articleDetails.setArticleId(art.getAid());
+            articleDetails.setArticleTitle(art.getTitle());
+            final String authorName = art.getCorrespondingAuthorFirstName()
+                    + art.getCorrespondingAuthorLastName();
+            articleDetails.setArticleAuthorName(authorName);
+            articleDetails.setArticleCoAuthors(parseCoAuthors(art));
+            articleDetails.setEmail(art.getCorrespondingAuthorEmail());
+            articleDetails.setArticleDoi(art.getDoi());
+            articleDetails.setAcceptanceDate(art.getAcceptedDt());
+            articleDetails.setIssue(art.getIssNum());
+            articleDetails.setVolume(art.getVolNum());
+            articleDetails.setEditorialRefCode(art.getEdRefCode());
+            articleDetails.setJpcmsInternalId(art.getJpcmsInternalId());
+            articleDetails.setPublicationDate(art.getPubYear());
+        }
+        return articleDetails;
+    }
+
+    /**
+     * Parses the co authors.
+     *
+     * @param art
+     *            the art
+     * @return the list
+     */
+    private final List<String> parseCoAuthors(final Art art) {
+        final List<CoAuthorDetails> coAuthorsList = art.getCoAuthors();
+        final List<String> coAuthors = new ArrayList<String>();
+        if (!StringUtils.isEmpty(coAuthorsList)) {
+            for (final CoAuthorDetails coAuthorDetails : coAuthorsList) {
+                final String coAuthorName = coAuthorDetails
+                        .getCoAuthorFirstName()
+                        + coAuthorDetails.getCoAuthorLastName();
+                coAuthors.add(coAuthorName);
+            }
+        }
+        return coAuthors;
+    }
+
+    /**
+     * Gets the journal data.
+     *
+     * @param getArticleDetails
+     *            the get article details
+     * @return the journal data
+     */
+    private final JournalDetails getJournalData(
+            final GetArticleDetails getArticleDetails) {
+        final Art art = getArticleDetails.getArt();
+        JournalDetails journalDetails = null;
+        if (!StringUtils.isEmpty(art)) {
+            journalDetails = new JournalDetails();
+            journalDetails.setJournalId(art.getJrnlId());
+            journalDetails.setJournalTitle(art.getJrnlTitle());
+            journalDetails.setJournalDoi(art.getJrnlDoi());
+            journalDetails.setJournalPrintIssn(art.getJrnlPissn());
+            journalDetails.setJournalElectronicIssn(art.getJrnlEissn());
+        }
+        return journalDetails;
+    }
+
+    /**
+     * Gets the publication data.
+     *
+     * @param getArticleDetails
+     *            the get article details
+     * @return the publication data
+     */
+    private final PublicationDetails getPublicationData(
+            final GetArticleDetails getArticleDetails) {
+        final Art art = getArticleDetails.getArt();
+        PublicationDetails publicationDetails = null;
+        if (!StringUtils.isEmpty(art)) {
+            publicationDetails = new PublicationDetails();
+            publicationDetails.setModifiedDate(art.getPubYear());
+            publicationDetails.setPublicationStatus("Make OO");
+            publicationDetails.setPublicationPath("From AS 2.0");
+        }
+        return publicationDetails;
+    }
+
+    /**
+     * Gets the order id.
+     *
+     * @return the order id
+     */
+    private final OrderDetails getOrderId() {
+        final OrderDetails orderData = new OrderDetails();
+        orderData.setOrderId("11232");
+        return orderData;
+    }
+
+    /**
+     * Gets the license data.
+     *
+     * @param getArticleDetails
+     *            the get article details
+     * @return the license data
+     */
+    private final LicenseDetails getLicenseData(
+            final GetArticleDetails getArticleDetails) {
+        final LastSignedLicense lastSignedLicense = getArticleDetails
+                .getLastSignedLicense();
+        LicenseDetails licenseDetails = null;
+        if (!StringUtils.isEmpty(lastSignedLicense)) {
+            licenseDetails = new LicenseDetails();
+            licenseDetails.setLicenseSignedDate(lastSignedLicense
+                    .getLicenseSignDate());
+            licenseDetails.setLicenseStatus(lastSignedLicense
+                    .getSignedElectronically());
+        }
+        return licenseDetails;
     }
 
     /**
