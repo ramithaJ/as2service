cc2cca6f0e9ae3a541850b3c4375881ee9d5f32f
Sarma <skpalli@wiley.com>
2015-07-28 22:27:50 +0530

    AS2-286:11489 - Create User Role: Roles Shared Service Integration

--- model/src/main/java/com/wiley/gr/ace/authorservices/model/external/UserData.java
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/external/UserData.java
@@ -1,14 +1,30 @@
 package com.wiley.gr.ace.authorservices.model.external;
 
+/**
+ * The Class UserData.
+ * 
+ * @author virtusa version 1.0
+ */
 public class UserData {
 	
+	/** The user id. */
 	private String userId;
 
+	/**
+	 * Gets the user id.
+	 *
+	 * @return the user id
+	 */
 	public String getUserId() {
 		return userId;
 	}
 
-	public void setUserId(String userId) {
+	/**
+	 * Sets the user id.
+	 *
+	 * @param userId the new user id
+	 */
+	public void setUserId(final String userId) {
 		this.userId = userId;
 	}
 
