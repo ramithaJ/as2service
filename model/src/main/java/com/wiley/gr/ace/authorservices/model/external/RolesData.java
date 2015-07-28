2a2c84b462b4400b5c32e7f0dee935b453d7183e
Sarma <skpalli@wiley.com>
2015-07-28 22:27:30 +0530

    AS2-286:11489 - Create User Role: Roles Shared Service Integration

--- model/src/main/java/com/wiley/gr/ace/authorservices/model/external/RolesData.java
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/external/RolesData.java
@@ -2,69 +2,137 @@ package com.wiley.gr.ace.authorservices.model.external;
 
 import java.util.List;
 
+// TODO: Auto-generated Javadoc
 /**
- * @author SarmaKumarap
+ * The Class RolesData.
  *
+ * @author virtusa version 1.0
  */
 public class RolesData {
 	
+	/** The role id. */
 	private String roleId;
 	
+	/** The role name. */
 	private String  roleName;
 	
+	/** The role description. */
 	private String roleDescription;
 	
+	/** The role type. */
 	private String roleType;
 	
+	/** The permissions. */
 	private List<PermissionData> permissions;
 	
+	/** The users. */
 	private List<UserData> users;
 
+	/**
+	 * Gets the role id.
+	 *
+	 * @return the role id
+	 */
 	public String getRoleId() {
 		return roleId;
 	}
 
-	public void setRoleId(String roleId) {
+	/**
+	 * Sets the role id.
+	 *
+	 * @param roleId the new role id
+	 */
+	public void setRoleId(final String roleId) {
 		this.roleId = roleId;
 	}
 
+	/**
+	 * Gets the role name.
+	 *
+	 * @return the role name
+	 */
 	public String getRoleName() {
 		return roleName;
 	}
 
-	public void setRoleName(String roleName) {
+	/**
+	 * Sets the role name.
+	 *
+	 * @param roleName the new role name
+	 */
+	public void setRoleName(final String roleName) {
 		this.roleName = roleName;
 	}
 
+	/**
+	 * Gets the role description.
+	 *
+	 * @return the role description
+	 */
 	public String getRoleDescription() {
 		return roleDescription;
 	}
 
-	public void setRoleDescription(String roleDescription) {
+	/**
+	 * Sets the role description.
+	 *
+	 * @param roleDescription the new role description
+	 */
+	public void setRoleDescription(final String roleDescription) {
 		this.roleDescription = roleDescription;
 	}
 
+	/**
+	 * Gets the role type.
+	 *
+	 * @return the role type
+	 */
 	public String getRoleType() {
 		return roleType;
 	}
 
-	public void setRoleType(String roleType) {
+	/**
+	 * Sets the role type.
+	 *
+	 * @param roleType the new role type
+	 */
+	public void setRoleType(final String roleType) {
 		this.roleType = roleType;
 	}
 
+	/**
+	 * Gets the permissions.
+	 *
+	 * @return the permissions
+	 */
 	public List<PermissionData> getPermissions() {
 		return permissions;
 	}
 
-	public void setPermissions(List<PermissionData> permissions) {
+	/**
+	 * Sets the permissions.
+	 *
+	 * @param permissions the new permissions
+	 */
+	public void setPermissions(final List<PermissionData> permissions) {
 		this.permissions = permissions;
 	}
 
+	/**
+	 * Gets the users.
+	 *
+	 * @return the users
+	 */
 	public List<UserData> getUsers() {
 		return users;
 	}
 
-	public void setUsers(List<UserData> users) {
+	/**
+	 * Sets the users.
+	 *
+	 * @param users the new users
+	 */
+	public void setUsers(final List<UserData> users) {
 		this.users = users;
 	}
 
