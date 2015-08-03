6f6a7112090b5932c21fe4f0d01c5eb63e25bb98
Sarma <skpalli@wiley.com>
2015-08-03 14:08:06 +0530

    AS2-286:11489 - Create User Role: Roles Shared Service Integration

--- externalservice/src/main/java/com/wiley/gr/ace/authorservices/externalservices/service/impl/RolesServiceImpl.java
+++ externalservice/src/main/java/com/wiley/gr/ace/authorservices/externalservices/service/impl/RolesServiceImpl.java
@@ -19,7 +19,10 @@ import org.springframework.beans.factory.annotation.Value;
 import com.wiley.gr.ace.authorservices.external.util.RestServiceInvokerUtil;
 import com.wiley.gr.ace.authorservices.externalservices.service.RolesService;
 import com.wiley.gr.ace.authorservices.model.external.PermissionData;
+import com.wiley.gr.ace.authorservices.model.external.PermissionsRequest;
+import com.wiley.gr.ace.authorservices.model.external.RoleRequest;
 import com.wiley.gr.ace.authorservices.model.external.RolesData;
+import com.wiley.gr.ace.authorservices.model.external.Status;
 
 /**
  * The Class RolesServiceImpl.
@@ -58,4 +61,63 @@ public class RolesServiceImpl implements RolesService {
         return Arrays.asList(permissionsArray);
     }
 
+    /* (non-Javadoc)
+     * @see com.wiley.gr.ace.authorservices.externalservices.service.RolesService#addRole(com.wiley.gr.ace.authorservices.model.external.RoleRequest)
+     */
+    @Override
+    public void addRole(final RoleRequest roleRequest) {
+        
+        RestServiceInvokerUtil.restServiceInvoker(rolesUrl, roleRequest, Status.class);
+        
+    }
+
+    /* (non-Javadoc)
+     * @see com.wiley.gr.ace.authorservices.externalservices.service.RolesService#addPermissions(com.wiley.gr.ace.authorservices.model.external.PermissionsRequest)
+     */
+    @Override
+    public void addPermissions(final PermissionsRequest permissionsRequest) {
+        
+        RestServiceInvokerUtil.restServiceInvoker(rolesUrl, permissionsRequest, Status.class);
+        
+    }
+
+    /* (non-Javadoc)
+     * @see com.wiley.gr.ace.authorservices.externalservices.service.RolesService#updateRole(com.wiley.gr.ace.authorservices.model.external.RoleRequest, java.lang.String)
+     */
+    @Override
+    public void updateRole(final RoleRequest roleRequest, final String roleId) {
+        
+        RestServiceInvokerUtil.putServiceData(rolesUrl+"/"+roleId, roleRequest);
+        
+    }
+
+    /* (non-Javadoc)
+     * @see com.wiley.gr.ace.authorservices.externalservices.service.RolesService#updatePermissions(com.wiley.gr.ace.authorservices.model.external.PermissionsRequest, java.lang.String)
+     */
+    @Override
+    public void updatePermissions(final List<PermissionsRequest> permissionsRequestList, final String roleId) {
+        
+        RestServiceInvokerUtil.putServiceData(rolesUrl+"/"+roleId+"/permissions", permissionsRequestList.toArray());
+        
+    }
+
+    /* (non-Javadoc)
+     * @see com.wiley.gr.ace.authorservices.externalservices.service.RolesService#getRolePermissions(java.lang.String)
+     */
+    @Override
+    public List<PermissionData> getRolePermissions(final String roleId) {
+        
+        PermissionData[] permissionsArray = (PermissionData[]) RestServiceInvokerUtil.getServiceData(rolesUrl, RolesData[].class);
+        
+        return Arrays.asList(permissionsArray);
+    }
+
+    /* (non-Javadoc)
+     * @see com.wiley.gr.ace.authorservices.externalservices.service.RolesService#deleteRolePermission(java.lang.String, java.lang.String)
+     */
+    @Override
+    public void deleteRolePermission(final String roleId, final String permissionCd) {
+        RestServiceInvokerUtil.deleteServiceData(rolesUrl+"/"+roleId+"/permissions"+permissionCd);
+    }
+    
 }
