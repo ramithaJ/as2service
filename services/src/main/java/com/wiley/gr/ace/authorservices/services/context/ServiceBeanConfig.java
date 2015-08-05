97b9de6c2575744cdd6c1215fa7e5c9ea7c1486e
Dishari <dide@wiley.com>
2015-08-05 18:05:08 +0530

    AS2-287 | dide | sign license

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/context/ServiceBeanConfig.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/context/ServiceBeanConfig.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-` * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
+ * Copyright (c) 2015 John Wiley & Sons, Inc. All rights reserved.
  *
  * All material contained herein is proprietary to John Wiley & Sons 
  * and its third party suppliers, if any. The methods, techniques and 
@@ -9,6 +9,9 @@
  * is strictly forbidden except by express prior written permission 
  * of John Wiley & Sons.
  *******************************************************************************/
+/**
+ * 
+ */
 package com.wiley.gr.ace.authorservices.services.context;
 
 import org.springframework.context.annotation.Bean;
@@ -20,6 +23,7 @@ import com.wiley.gr.ace.authorservices.services.service.AdminLoginService;
 import com.wiley.gr.ace.authorservices.services.service.ArticleAssignmentService;
 import com.wiley.gr.ace.authorservices.services.service.AuthorProfileService;
 import com.wiley.gr.ace.authorservices.services.service.DashboardService;
+import com.wiley.gr.ace.authorservices.services.service.LicenseService;
 import com.wiley.gr.ace.authorservices.services.service.OnlineOpenAuthorValidatorService;
 import com.wiley.gr.ace.authorservices.services.service.OpenAccessService;
 import com.wiley.gr.ace.authorservices.services.service.OrcidService;
@@ -35,6 +39,7 @@ import com.wiley.gr.ace.authorservices.services.service.impl.AdminLoginServiceIm
 import com.wiley.gr.ace.authorservices.services.service.impl.ArticleAssignmentServiceImpl;
 import com.wiley.gr.ace.authorservices.services.service.impl.AuthorProfileServiceImpl;
 import com.wiley.gr.ace.authorservices.services.service.impl.DashboardServiceImpl;
+import com.wiley.gr.ace.authorservices.services.service.impl.LicenseServiceImpl;
 import com.wiley.gr.ace.authorservices.services.service.impl.OnlineOpenAuthorValidatorServiceImpl;
 import com.wiley.gr.ace.authorservices.services.service.impl.OpenAccessServiceImpl;
 import com.wiley.gr.ace.authorservices.services.service.impl.OrcidServiceImpl;
@@ -47,153 +52,172 @@ import com.wiley.gr.ace.authorservices.services.service.impl.UserLoginServiceImp
 
 /**
  * The Class ServiceBeanConfig.
+ * 
+ * @author virtusa version 1.0
  */
 @Configuration
 public class ServiceBeanConfig {
 
-	/**
-	 * Adminloginservice.
-	 *
-	 * @return the admin login service
-	 */
-	@Bean(name = "AdminLoginService")
-	public AdminLoginService adminloginservice() {
-		return new AdminLoginServiceImpl();
-	}
+    /**
+     * Adminloginservice.
+     *
+     * @return the admin login service
+     */
+    @Bean(name = "AdminLoginService")
+    public AdminLoginService adminloginservice() {
+        return new AdminLoginServiceImpl();
+    }
 
-	/**
-	 * User login service.
-	 *
-	 * @return the user login service
-	 */
-	@Bean(name = "UserLoginService")
-	public UserLoginService userLoginService() {
-		return new UserLoginServiceImpl();
-	}
+    /**
+     * User login service.
+     *
+     * @return the user login service
+     */
+    @Bean(name = "UserLoginService")
+    public UserLoginService userLoginService() {
+        return new UserLoginServiceImpl();
+    }
 
-	/**
-	 * Registration service.
-	 *
-	 * @return the registration service
-	 */
-	@Bean(name = "RegistrationService")
-	public RegistrationService registrationService() {
+    /**
+     * Registration service.
+     *
+     * @return the registration service
+     */
+    @Bean(name = "RegistrationService")
+    public RegistrationService registrationService() {
 
-		return new RegistrationServiceImpl();
-	}
+        return new RegistrationServiceImpl();
+    }
 
-	/**
-	 * Dashboard service.
-	 *
-	 * @return the dashboard service
-	 */
-	@Bean(name = "DashboardService")
-	public DashboardService dashboardService() {
-		return new DashboardServiceImpl();
-	}
+    /**
+     * Dashboard service.
+     *
+     * @return the dashboard service
+     */
+    @Bean(name = "DashboardService")
+    public DashboardService dashboardService() {
+        return new DashboardServiceImpl();
+    }
 
-	/**
-	 * Update user service.
-	 *
-	 * @return the update user service
-	 */
-	@Bean(name = "UpdateUserService")
-	public UpdateUserService updateUserService() {
-		return new UpdateUserServiceImpl();
-	}
+    /**
+     * Update user service.
+     *
+     * @return the update user service
+     */
+    @Bean(name = "UpdateUserService")
+    public UpdateUserService updateUserService() {
+        return new UpdateUserServiceImpl();
+    }
 
-	/**
-	 * Orcid service.
-	 *
-	 * @return the orcid service
-	 */
-	@Bean(name = "OrcidService")
-	public OrcidService orcidService() {
-		return new OrcidServiceImpl();
-	}
+    /**
+     * Orcid service.
+     *
+     * @return the orcid service
+     */
+    @Bean(name = "OrcidService")
+    public OrcidService orcidService() {
+        return new OrcidServiceImpl();
+    }
 
-	/**
-	 * User account service.
-	 *
-	 * @return the user account service
-	 */
-	@Bean(name = "UserAccountService")
-	public UserAccountService userAccountService() {
-		return new UserAccountServiceImpl();
-	}
+    /**
+     * User account service.
+     *
+     * @return the user account service
+     */
+    @Bean(name = "UserAccountService")
+    public UserAccountService userAccountService() {
+        return new UserAccountServiceImpl();
+    }
 
-	/**
-	 * A s data service.
-	 *
-	 * @return the AS data service
-	 */
-	@Bean(name = "ASDataService")
-	public ASDataService aSDataService() {
-		return new ASDataServiceImpl();
-	}
+    /**
+     * A s data service.
+     *
+     * @return the AS data service
+     */
+    @Bean(name = "ASDataService")
+    public ASDataService aSDataService() {
+        return new ASDataServiceImpl();
+    }
 
-	/**
-	 * Author profile service.
-	 *
-	 * @return the author profile service
-	 */
-	@Bean(name = "AuthorProfileService")
-	public AuthorProfileService authorProfileService() {
+    /**
+     * Author profile service.
+     *
+     * @return the author profile service
+     */
+    @Bean(name = "AuthorProfileService")
+    public AuthorProfileService authorProfileService() {
 
-		return new AuthorProfileServiceImpl();
-	}
+        return new AuthorProfileServiceImpl();
+    }
 
-	/**
-	 * Article assignment service.
-	 *
-	 * @return the article assignment service
-	 */
-	@Bean(name = "ArticleAssignmentService")
-	public ArticleAssignmentService articleAssignmentService() {
-		return new ArticleAssignmentServiceImpl();
-	}
+    /**
+     * Article assignment service.
+     *
+     * @return the article assignment service
+     */
+    @Bean(name = "ArticleAssignmentService")
+    public ArticleAssignmentService articleAssignmentService() {
+        return new ArticleAssignmentServiceImpl();
+    }
 
-	/**
-	 * Order online open service.
-	 *
-	 * @return the order online open service
-	 */
-	@Bean(name = "OrderOnlineOpenService")
-	public OrderOnlineOpenService orderOnlineOpenService() {
-		return new OrderOnlineOpenServiceImpl();
-	}
+    /**
+     * Order online open service.
+     *
+     * @return the order online open service
+     */
+    @Bean(name = "OrderOnlineOpenService")
+    public OrderOnlineOpenService orderOnlineOpenService() {
+        return new OrderOnlineOpenServiceImpl();
+    }
 
-	@Bean(name = "OpenAccessService")
-	public OpenAccessService openAccessService() {
-		return new OpenAccessServiceImpl();
-	}
+    /**
+     * Open access service.
+     *
+     * @return the open access service
+     */
+    @Bean(name = "OpenAccessService")
+    public OpenAccessService openAccessService() {
+        return new OpenAccessServiceImpl();
+    }
 
-	/**
-	 * Online Open Author Validator Service
-	 * 
-	 * @return OnlineOpenAuthorValidatorService
-	 */
-	@Bean(name = "OnlineOpenAuthorValidatorService")
-	public OnlineOpenAuthorValidatorService onlineOpenAuthorValidatorService() {
-		return new OnlineOpenAuthorValidatorServiceImpl();
-	}
+    /**
+     * Online Open Author Validator Service.
+     *
+     * @return OnlineOpenAuthorValidatorService
+     */
+    @Bean(name = "OnlineOpenAuthorValidatorService")
+    public OnlineOpenAuthorValidatorService onlineOpenAuthorValidatorService() {
+        return new OnlineOpenAuthorValidatorServiceImpl();
+    }
 
-	/**
-	 * SaveArticleData
-	 * 
-	 * @return
-	 */
-	@Bean(name = "SaveArticleData")
-	public SaveArticleData saveArticleData() {
-		return new SaveArticleDataImpl();
-	}
-	
-	/**
-	 * @return
-	 */
-	@Bean(name = "AdditionalPermissionService")
-	public AdditionalPermissionService additionalPermissionService() {
-		return new AdditionalPermissionServiceImpl();
-	}
-	
+    /**
+     * SaveArticleData.
+     *
+     * @return the save article data
+     */
+    @Bean(name = "SaveArticleData")
+    public SaveArticleData saveArticleData() {
+        return new SaveArticleDataImpl();
+    }
+
+    /**
+     * Additional permission service.
+     *
+     * @return the additional permission service
+     */
+    @Bean(name = "AdditionalPermissionService")
+    public AdditionalPermissionService additionalPermissionService() {
+        return new AdditionalPermissionServiceImpl();
+    }
+
+    /**
+     * License service.
+     *
+     * @return the license service
+     */
+    @Bean(name = "LicenseService")
+    public LicenseService licenseService() {
+        return new LicenseServiceImpl();
+    }
+
 }
