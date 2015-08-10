4a3867de30f36553819b887c1e9201116e662c2d
Sripad Saraf <saraf.plip@gmail.com>
2015-08-11 01:07:08 +0530

    AS2-142 | ssaraf | Auto complete feature for funder service

---
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/SubFunder.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.wiley.gr.ace.authorservices.model;
+
+import java.io.Serializable;
+
+/**
+ * SubFunder.
+ *
+ * @author virtusa
+ * @version 1.0
+ */
+public class SubFunder implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	/** The id **/
+	private String id;
+
+	/** The doi **/
+	private String doi;
+
+	/** The name **/
+	private String name;
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the doi
+	 */
+	public String getDoi() {
+		return doi;
+	}
+
+	/**
+	 * @param doi
+	 *            the doi to set
+	 */
+	public void setDoi(String doi) {
+		this.doi = doi;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
