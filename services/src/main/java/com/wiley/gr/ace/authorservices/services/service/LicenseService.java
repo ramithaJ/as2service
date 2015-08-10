f7a5b4122a270c682d5cc5a13ac106705025e64f
Dishari <dide@wiley.com>
2015-08-10 11:48:49 +0530

    AS2-287 | dide | sign OA license

--- services/src/main/java/com/wiley/gr/ace/authorservices/services/service/LicenseService.java
+++ services/src/main/java/com/wiley/gr/ace/authorservices/services/service/LicenseService.java
@@ -27,66 +27,69 @@ import com.wiley.gr.ace.authorservices.model.LicenseStatus;
  */
 public interface LicenseService {
 
-	/**
-	 * Gets the license choice.
-	 *
-	 * @param dhId
-	 *            the dh id
-	 * @param licenseObject
-	 *            the license object
-	 * @return the license choice
-	 */
-	ArrayList<String> getLicenseChoice(String dhId, LicenseObject licenseObject);
+    /**
+     * Gets the license choice.
+     *
+     * @param dhId
+     *            the dh id
+     * @param licenseObject
+     *            the license object
+     * @return the license choice
+     */
+    ArrayList<String> getLicenseChoice(String dhId, LicenseObject licenseObject);
 
-	/**
-	 * Gets the license text.
-	 *
-	 * @param licenseObject
-	 *            the license object
-	 * @return the license text
-	 */
-	String getLicenseText(LicenseObject licenseObject);
+    /**
+     * Gets the license text.
+     *
+     * @param licenseObject
+     *            the license object
+     * @return the license text
+     */
+    String getLicenseText(LicenseObject licenseObject);
 
-	/**
-	 * Gets the license copy.
-	 *
-	 * @param dhId
-	 *            the dh id
-	 * @return the license copy
-	 */
-	File getLicenseCopy(String dhId);
+    /**
+     * Gets the license copy.
+     *
+     * @param dhId
+     *            the dh id
+     * @return the license copy
+     */
+    File getLicenseCopy(String dhId);
 
-	/**
-	 * Save license later.
-	 *
-	 * @param licenseObject
-	 *            the license object
-	 * @param userId
-	 *            the user id
-	 * @param articleId
-	 *            the article id
-	 */
-	void saveLicenseLater(LicenseObject licenseObject, String userId,
-			String articleId);
+    /**
+     * Save license later.
+     *
+     * @param licenseObject
+     *            the license object
+     * @param userId
+     *            the user id
+     * @param articleId
+     *            the article id
+     * @return the integer
+     */
+    Integer saveLicenseLater(LicenseObject licenseObject, String userId,
+            String articleId);
 
-	/**
-	 * Sign license.
-	 *
-	 * @param licenseObject
-	 *            the license object
-	 * @param dhId
-	 *            the dh id
-	 * @return the string
-	 */
-	String signLicense(LicenseObject licenseObject, String dhId);
+    /**
+     * Sign license.
+     *
+     * @param licenseObject
+     *            the license object
+     * @param dhId
+     *            the dh id
+     * @return the string
+     */
+    String signLicense(LicenseObject licenseObject, String dhId);
 
-	/**
-	 * Gets the license status.
-	 *
-	 * @param dhId the dh id
-	 * @param userId the user id
-	 * @return the license status
-	 */
-	LicenseStatus getLicenseStatus(String dhId, String userId);
+    /**
+     * Gets the license status.
+     *
+     * @param dhId
+     *            the dh id
+     * @param userId
+     *            the user id
+     * @return the license status
+     */
+    LicenseStatus getLicenseStatus(String dhId, String userId);
 
 }
