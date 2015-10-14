09d811a68a221afc66c646d187c29ccb663f7873
Sarma <skpalli@wiley.com>
2015-10-14 17:48:40 +0530

    AS2-629 | skpalli | User Login

--- model/src/main/java/com/wiley/gr/ace/authorservices/model/UserLogin.java
+++ model/src/main/java/com/wiley/gr/ace/authorservices/model/UserLogin.java
@@ -13,43 +13,50 @@
  */
 package com.wiley.gr.ace.authorservices.model;
 
+// TODO: Auto-generated Javadoc
 /**
- * @author virtusa version1.0
+ * The Class UserLogin.
  *
+ * @author virtusa version1.0
  */
 public class UserLogin {
 
-    /**
-     * This field holds the value of userId
-     */
-    private Integer userId;
+    /** This field holds the value of userId. */
+    private String userId;
 
-    /**
-     * This field holds the value of firstName
-     */
+    /** This field holds the value of firstName. */
     private String firstName;
 
-    /**
-     * This field holds the value of lastName
-     */
+    /** This field holds the value of lastName. */
     private String lastName;
+    
+    /** The alm user id. */
+    private String almUserId;
+    
+    /** The orcid id. */
+    private String orcidId;
 
     /**
+     * Gets the user id.
+     *
      * @return the userId
      */
-    public final Integer getUserId() {
+    public final String getUserId() {
         return userId;
     }
 
     /**
-     * @param userId
-     *            the userId to set
+     * Sets the user id.
+     *
+     * @param userId            the userId to set
      */
-    public final void setUserId(final Integer userId) {
+    public final void setUserId(final String userId) {
         this.userId = userId;
     }
 
     /**
+     * Gets the first name.
+     *
      * @return the firstName
      */
     public final String getFirstName() {
@@ -57,14 +64,17 @@ public class UserLogin {
     }
 
     /**
-     * @param firstName
-     *            the firstName to set
+     * Sets the first name.
+     *
+     * @param firstName            the firstName to set
      */
     public final void setFirstName(final String firstName) {
         this.firstName = firstName;
     }
 
     /**
+     * Gets the last name.
+     *
      * @return the lastName
      */
     public final String getLastName() {
@@ -72,11 +82,48 @@ public class UserLogin {
     }
 
     /**
-     * @param lastName
-     *            the lastName to set
+     * Sets the last name.
+     *
+     * @param lastName            the lastName to set
      */
     public final void setLastName(final String lastName) {
         this.lastName = lastName;
     }
+
+	/**
+	 * Gets the alm user id.
+	 *
+	 * @return the alm user id
+	 */
+	public String getAlmUserId() {
+		return almUserId;
+	}
+
+	/**
+	 * Sets the alm user id.
+	 *
+	 * @param almUserId the new alm user id
+	 */
+	public void setAlmUserId(String almUserId) {
+		this.almUserId = almUserId;
+	}
+
+	/**
+	 * Gets the orcid id.
+	 *
+	 * @return the orcid id
+	 */
+	public String getOrcidId() {
+		return orcidId;
+	}
+
+	/**
+	 * Sets the orcid id.
+	 *
+	 * @param orcidId the new orcid id
+	 */
+	public void setOrcidId(String orcidId) {
+		this.orcidId = orcidId;
+	}
 
 }
